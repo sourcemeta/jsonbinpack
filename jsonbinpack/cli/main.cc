630821b423178ef98069e8b5e30de0de2e2aa055
Juan Cruz Viotti <jv@jviotti.com>
2021-10-16 18:47:21 -0400

    Recognize unknown CLI commands
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- jsonbinpack/cli/main.cc
+++ jsonbinpack/cli/main.cc
@@ -1,24 +1,29 @@
 // Copyright (c) 2021 Juan Cruz Viotti. All rights reserved.
 
+#include <iostream>
 #include <string>
 
 #include "jsonbinpack/cli/command_help.h"
 #include "jsonbinpack/cli/command_version.h"
 
 static const char *kCommandVersion = "version";
+static const char *kCommandHelp = "help";
 
 int main(int argc, char **argv) {
-  const std::string_view command{argc <= 1 ? "help" : argv[1]};
-  if (command == kCommandVersion) {
-    return jsonbinpack::cli::command::Version();
-  }
-
-  const int code = jsonbinpack::cli::command::Help(argv[0]);
   // This means the user executed the tool without passing any argument,
   // in which case a non-zero exit code is more appropriate.
-  if (code == 0 && argc == 1) {
+  if (argc <= 1) {
+    jsonbinpack::cli::command::Help(argv[0]);
     return 1;
   }
 
-  return code;
+  const std::string_view command{argv[1]};
+  if (command == kCommandVersion) {
+    return jsonbinpack::cli::command::Version();
+  } else if (command == kCommandHelp) {
+    return jsonbinpack::cli::command::Help(argv[0]);
+  } else {
+    std::cerr << "Unknown command: " << command << "\n";
+    return 1;
+  }
 }
