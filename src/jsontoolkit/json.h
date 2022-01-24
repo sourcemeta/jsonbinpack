d5ea17e1b1a51deea283d4f1e124b6701051d52a
Juan Cruz Viotti <jv@jviotti.com>
2022-01-24 19:29:31 -0400

    Better RapidJSON support
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsontoolkit/json.h
+++ src/jsontoolkit/json.h
@@ -2,7 +2,10 @@
 #define SOURCEMETA_JSONTOOLKIT_JSON_H_
 
 #include <string>
+
+#ifdef SOURCEMETA_JSONTOOLKIT_RAPIDJSON
 #include <rapidjson/document.h>
+#endif
 
 namespace sourcemeta {
   namespace jsontoolkit {
@@ -11,7 +14,9 @@ namespace sourcemeta {
       public:
         JSON(const std::string &json);
       private:
+#ifdef SOURCEMETA_JSONTOOLKIT_RAPIDJSON
         rapidjson::Document document;
+#endif
     };
 
   }
