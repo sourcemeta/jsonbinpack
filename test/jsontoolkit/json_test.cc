9fb6e4174d313b743be98f20790f0b9fadc2f4a4
Juan Cruz Viotti <jv@jviotti.com>
2022-02-02 20:02:23 -0400

    Refactor tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/jsontoolkit/json_test.cc
+++ test/jsontoolkit/json_test.cc
@@ -1,21 +1,21 @@
 #include <gtest/gtest.h>
 #include <vector> // std::vector
-#include <functional> // std::reference_wrapper
+#include <string_view> // std::string_view
 #include <jsontoolkit/json.h>
 
-TEST(Boolean, true_bool) {
+TEST(JSON, true_bool) {
   sourcemeta::jsontoolkit::JSON document {true};
   EXPECT_TRUE(document.is_boolean());
   EXPECT_TRUE(document.to_boolean());
 }
 
-TEST(Boolean, false_bool) {
+TEST(JSON, false_bool) {
   sourcemeta::jsontoolkit::JSON document {false};
   EXPECT_TRUE(document.is_boolean());
   EXPECT_FALSE(document.to_boolean());
 }
 
-TEST(Boolean, set) {
+TEST(JSON, set_boolean) {
   sourcemeta::jsontoolkit::JSON document {false};
   EXPECT_TRUE(document.is_boolean());
   EXPECT_FALSE(document.to_boolean());
@@ -27,39 +27,10 @@ TEST(Boolean, set) {
   EXPECT_FALSE(document.to_boolean());
 }
 
-TEST(Array, BooleanArrayAt) {
-  sourcemeta::jsontoolkit::Array<sourcemeta::jsontoolkit::JSON> document {"[true,false,true]"};
-  EXPECT_EQ(document.size(), 3);
-  EXPECT_TRUE(document.at(0).to_boolean());
-  EXPECT_FALSE(document.at(1).to_boolean());
-  EXPECT_TRUE(document.at(2).to_boolean());
-}
-
-TEST(JSON, BooleanTrue) {
-  sourcemeta::jsontoolkit::JSON document {true};
-  EXPECT_TRUE(document.is_boolean());
-  EXPECT_TRUE(document.to_boolean());
-  EXPECT_FALSE(document.is_array());
-}
-
-TEST(JSON, BooleanFalse) {
-  sourcemeta::jsontoolkit::JSON document {false};
-  EXPECT_TRUE(document.is_boolean());
-  EXPECT_FALSE(document.to_boolean());
-  EXPECT_FALSE(document.is_array());
-}
-
-TEST(JSON, BooleanConstructor) {
-  sourcemeta::jsontoolkit::Boolean<sourcemeta::jsontoolkit::JSON> value {true};
-  sourcemeta::jsontoolkit::JSON document {value};
-  EXPECT_TRUE(document.is_boolean());
-  EXPECT_TRUE(document.to_boolean());
-}
-
-TEST(JSON, BooleanArrayAt) {
-  sourcemeta::jsontoolkit::Array<sourcemeta::jsontoolkit::JSON> value {"[true,false,true]"};
-  sourcemeta::jsontoolkit::JSON document {value};
+TEST(JSON, at_boolean) {
+  sourcemeta::jsontoolkit::JSON document {std::string_view("[true,false,true]")};
   EXPECT_TRUE(document.is_array());
+  EXPECT_EQ(document.size(), 3);
 
   EXPECT_TRUE(document.at(0).is_boolean());
   EXPECT_TRUE(document.at(1).is_boolean());
@@ -70,11 +41,12 @@ TEST(JSON, BooleanArrayAt) {
   EXPECT_TRUE(document.at(2).to_boolean());
 }
 
-TEST(JSON, BooleanArrayIterator) {
-  sourcemeta::jsontoolkit::Array<sourcemeta::jsontoolkit::JSON> value {"[true,false,false]"};
+TEST(JSON, boolean_array_iterator) {
+  sourcemeta::jsontoolkit::JSON value {std::string_view("[true,false,false]")};
   std::vector<bool> result;
 
-  for (sourcemeta::jsontoolkit::JSON &element : value) {
+  // TODO: Can we make range for loops nicer?
+  for (sourcemeta::jsontoolkit::JSON &element : *(value.to_array())) {
     result.push_back(element.to_boolean());
   }
 
@@ -84,12 +56,13 @@ TEST(JSON, BooleanArrayIterator) {
   EXPECT_FALSE(result.at(2));
 }
 
-TEST(JSON, BooleanArrayReverseIterator) {
-  sourcemeta::jsontoolkit::Array<sourcemeta::jsontoolkit::JSON> value {"[true,false,false]"};
+TEST(JSON, boolean_array_reverse_iterator) {
+  sourcemeta::jsontoolkit::JSON value {std::string_view("[true,false,false]")};
   std::vector<bool> result;
 
+  // TODO: Can we hide the Array type somehow?
   for (sourcemeta::jsontoolkit::Array<sourcemeta::jsontoolkit::JSON>::reverse_iterator
-      iterator = value.rbegin(); iterator != value.rend(); iterator++) {
+      iterator = value.to_array()->rbegin(); iterator != value.to_array()->rend(); iterator++) {
     result.push_back(iterator->to_boolean());
   }
 
@@ -99,7 +72,7 @@ TEST(JSON, BooleanArrayReverseIterator) {
   EXPECT_TRUE(result.at(2));
 }
 
-TEST(JSON, ArrayParsePadded) {
+TEST(JSON, array_padded_parse) {
   sourcemeta::jsontoolkit::JSON value {std::string_view("  [true,false,false]  ")};
   EXPECT_TRUE(value.is_array());
 }
