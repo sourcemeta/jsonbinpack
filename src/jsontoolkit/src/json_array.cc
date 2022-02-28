4dfd0e97e3c1664673723603b1e618b9b27836e5
Juan Cruz Viotti <jv@jviotti.com>
2022-02-28 17:42:19 -0400

    Improve array parsing definition
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsontoolkit/src/json_array.cc
+++ src/jsontoolkit/src/json_array.cc
@@ -1,5 +1,6 @@
 #include <jsontoolkit/json.h>
 #include <jsontoolkit/json_array.h>
+#include <stdexcept> // std::domain_error
 
 #include "parsers/parser.h"
 
@@ -29,7 +30,107 @@ auto sourcemeta::jsontoolkit::GenericArray<Wrapper>::size() ->
 
 template <typename Wrapper>
 auto sourcemeta::jsontoolkit::GenericArray<Wrapper>::parse_source() -> void {
-  this->data = sourcemeta::jsontoolkit::parser::array<Wrapper>(this->source());
+  const std::string_view document{
+      sourcemeta::jsontoolkit::parser::trim(this->source())};
+  if (document.front() !=
+          sourcemeta::jsontoolkit::GenericArray<Wrapper>::token_begin ||
+      document.back() !=
+          sourcemeta::jsontoolkit::GenericArray<Wrapper>::token_end) {
+    throw std::domain_error("Invalid array");
+  }
+
+  const std::string_view::size_type size{document.size()};
+  std::string_view::size_type element_start_index = 0;
+  std::string_view::size_type level = 0;
+  bool is_string = false;
+
+  for (std::string_view::size_type index = 1; index < size - 1; index++) {
+    std::string_view::const_reference character{document.at(index)};
+    const bool is_last_character = index == size - 2;
+
+    switch (character) {
+    case sourcemeta::jsontoolkit::GenericArray<Wrapper>::token_begin:
+      if (is_string) {
+        break;
+      }
+
+      // The start of an array at level 0 is by definition a new element
+      if (level == 0) {
+        element_start_index = index;
+      }
+
+      level += 1;
+      break;
+    case sourcemeta::jsontoolkit::GenericArray<Wrapper>::token_end:
+      if (is_string) {
+        break;
+      }
+
+      if (level == 0) {
+        throw std::domain_error("Unexpected right bracket");
+      }
+
+      level -= 1;
+
+      // Only push an element on a final right bracket
+      if (is_last_character && element_start_index > 0) {
+        this->data.push_back(Wrapper(document.substr(
+            element_start_index, index - element_start_index + 1)));
+        element_start_index = 0;
+      }
+
+      break;
+    case sourcemeta::jsontoolkit::GenericArray<Wrapper>::token_delimiter:
+      if (is_string) {
+        break;
+      }
+
+      if (element_start_index == 0) {
+        throw std::domain_error("Separator without a preceding element");
+      }
+
+      if (is_last_character) {
+        throw std::domain_error("Trailing comma");
+      }
+
+      if (level == 0) {
+        this->data.push_back(Wrapper(
+            document.substr(element_start_index, index - element_start_index)));
+        element_start_index = 0;
+      }
+
+      break;
+    default:
+      if (is_last_character && element_start_index > 0) {
+        this->data.push_back(Wrapper(document.substr(
+            element_start_index, index - element_start_index + 1)));
+        element_start_index = 0;
+      } else if (is_last_character && element_start_index == 0 &&
+                 !sourcemeta::jsontoolkit::parser::is_blank(character)) {
+        this->data.push_back(Wrapper(document.substr(index, 1)));
+      } else if (!sourcemeta::jsontoolkit::parser::is_blank(character) &&
+                 element_start_index == 0 && level == 0) {
+        element_start_index = index;
+      }
+
+      if (character == sourcemeta::jsontoolkit::String::token_begin ||
+          character == sourcemeta::jsontoolkit::String::token_end) {
+        if (is_string) {
+          is_string = false;
+        } else if (index == 0 ||
+                   document.at(index - 1) !=
+                       sourcemeta::jsontoolkit::String::token_escape) {
+          is_string = true;
+        }
+      }
+
+      break;
+    }
+  }
+
+  if (level > 0) {
+    throw std::domain_error("Unbalanced array");
+  }
 }
 
 template <typename Wrapper>
