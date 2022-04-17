7221141fea32e9ac6ab20325e27cb13f91226db6
Juan Cruz Viotti <jv@jviotti.com>
2022-04-17 13:22:45 -0300

    Add one more test
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/canonicalizer/schemas_test.cc
+++ test/canonicalizer/schemas_test.cc
@@ -3,16 +3,36 @@
 #include <jsontoolkit/json.h>
 
 TEST(Canonicalizer, max_contains_without_contains_1) {
-  sourcemeta::jsontoolkit::JSON document(R"EOF({
+  sourcemeta::jsontoolkit::JSON document(R"JSON({
     "type": "array",
     "maxContains": 2
-  })EOF");
+  })JSON");
 
   sourcemeta::jsonbinpack::canonicalizer::apply(document);
 
-  sourcemeta::jsontoolkit::JSON expected(R"EOF({
+  sourcemeta::jsontoolkit::JSON expected(R"JSON({
     "type": "array"
-  })EOF");
+  })JSON");
+
+  document.parse();
+  expected.parse();
+  EXPECT_EQ(document, expected);
+}
+
+TEST(Canonicalizer, max_contains_without_contains_2) {
+  sourcemeta::jsontoolkit::JSON document(R"JSON({
+    "type": "array",
+    "contains": { "type": "string" },
+    "maxContains": 2
+  })JSON");
+
+  sourcemeta::jsonbinpack::canonicalizer::apply(document);
+
+  sourcemeta::jsontoolkit::JSON expected(R"JSON({
+    "type": "array",
+    "contains": { "type": "string" },
+    "maxContains": 2
+  })JSON");
 
   document.parse();
   expected.parse();
