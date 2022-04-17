f6bcc4188f0c4f4edcac160c35aae9178e90259e
Juan Cruz Viotti <jv@jviotti.com>
2022-04-17 13:19:58 -0300

    Use JSON equality on canonicalizer tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/canonicalizer/schemas_test.cc
+++ test/canonicalizer/schemas_test.cc
@@ -2,13 +2,19 @@
 #include <jsonbinpack/canonicalizer/canonicalizer.h>
 #include <jsontoolkit/json.h>
 
-TEST(Canonicalizer, max_contains_without_contains) {
-  sourcemeta::jsontoolkit::JSON document(
-      "{\"type\":\"array\",\"maxContains\":2}");
+TEST(Canonicalizer, max_contains_without_contains_1) {
+  sourcemeta::jsontoolkit::JSON document(R"EOF({
+    "type": "array",
+    "maxContains": 2
+  })EOF");
+
   sourcemeta::jsonbinpack::canonicalizer::apply(document);
-  EXPECT_TRUE(document.is_object());
-  EXPECT_EQ(document.size(), 1);
-  EXPECT_TRUE(document.contains("type"));
-  EXPECT_TRUE(document["type"].is_string());
-  EXPECT_EQ(document["type"], "array");
+
+  sourcemeta::jsontoolkit::JSON expected(R"EOF({
+    "type": "array"
+  })EOF");
+
+  document.parse();
+  expected.parse();
+  EXPECT_EQ(document, expected);
 }
