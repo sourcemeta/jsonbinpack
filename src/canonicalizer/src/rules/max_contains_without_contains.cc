654ab6d9f1f803f6d507f06a878cfd1858372358
Juan Cruz Viotti <jv@jviotti.com>
2022-04-20 20:50:39 -0300

    [WIP] Check for schema vocabularies on canonicalizer
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/src/rules/max_contains_without_contains.cc
+++ src/canonicalizer/src/rules/max_contains_without_contains.cc
@@ -3,10 +3,19 @@
 
 namespace sourcemeta::jsonbinpack::canonicalizer::rules {
 
+/**
+ * schema.maxContains? && !schema.contains? => schema \ { maxContains }
+ */
+
+// TODO: Rules should also check what vocabulary its currently in use.
+// We can approximate this my creating a
+// sourcemeta::jsontoolkit::JSONSchema::has_vocabulary(); function that returns
+// true for all known vocabularies at the moment?
+
 class MaxContainsWithoutContains final
     : public sourcemeta::jsonbinpack::canonicalizer::Rule {
 public:
-  MaxContainsWithoutContains() : Rule("max_contains_without_contains"){};
+  MaxContainsWithoutContains() : Rule("MaxContainsWithoutContains"){};
   auto condition(sourcemeta::jsontoolkit::JSON &schema) -> bool override {
     return schema.contains("maxContains") && !schema.contains("contains");
   }
