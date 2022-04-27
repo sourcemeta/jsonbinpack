c55e7a34d96cd961d32c5aa3d56fe1016d9ce79f
Juan Cruz Viotti <jv@jviotti.com>
2022-04-26 20:58:22 -0400

    Add ContentSchemaWithoutContentMediaType rule (#152)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/canonicalizer/src/rules/content_schema_without_content_media_type.cc
@@ -0,0 +1,26 @@
+#include <jsonbinpack/canonicalizer/rule.h>
+#include <jsontoolkit/json.h>
+#include <jsontoolkit/schema.h>
+
+namespace sourcemeta::jsonbinpack::canonicalizer::rules {
+
+class ContentSchemaWithoutContentMediaType final
+    : public sourcemeta::jsonbinpack::canonicalizer::Rule {
+public:
+  ContentSchemaWithoutContentMediaType()
+      : Rule("content_schema_without_content_media_type"){};
+  [[nodiscard]] auto
+  condition(const sourcemeta::jsontoolkit::Schema &schema) const
+      -> bool override {
+    return schema.has_vocabulary(
+               "https://json-schema.org/draft/2020-12/vocab/content") &&
+           schema.contains("contentSchema") &&
+           !schema.contains("contentMediaType");
+  }
+
+  auto transform(sourcemeta::jsontoolkit::JSON &schema) -> void override {
+    schema.erase("contentSchema");
+  }
+};
+
+} // namespace sourcemeta::jsonbinpack::canonicalizer::rules
--- test/canonicalizer/schemas_test.cc
+++ test/canonicalizer/schemas_test.cc
@@ -18,7 +18,7 @@ TEST(Canonicalizer, max_contains_without_contains_1) {
 
   document.parse();
   expected.parse();
-  EXPECT_EQ(document, expected);
+  EXPECT_EQ(expected, document);
 }
 
 TEST(Canonicalizer, max_contains_without_contains_2) {
@@ -40,5 +40,26 @@ TEST(Canonicalizer, max_contains_without_contains_2) {
 
   document.parse();
   expected.parse();
-  EXPECT_EQ(document, expected);
+  EXPECT_EQ(expected, document);
+}
+
+TEST(Canonicalizer, content_schema_without_content_media_type_1) {
+  sourcemeta::jsontoolkit::JSON document(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "string",
+    "contentSchema": {
+      "type": "object"
+    }
+  })JSON");
+
+  sourcemeta::jsonbinpack::canonicalizer::apply(document);
+
+  sourcemeta::jsontoolkit::JSON expected(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "string"
+  })JSON");
+
+  document.parse();
+  expected.parse();
+  EXPECT_EQ(expected, document);
 }
