714908ad71429fca60ba48d4fdf91865178b65bc
Juan Cruz Viotti <jv@jviotti.com>
2022-06-08 18:31:50 -0400

    Implement BooleanAsEnum canonicalizer rule
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/canonicalizer/src/rules/boolean_as_enum.h
@@ -0,0 +1,32 @@
+#include <jsonbinpack/canonicalizer/rule.h>
+#include <jsontoolkit/json.h>
+#include <jsontoolkit/schema.h>
+
+#include <vector>  // std::vector
+
+namespace sourcemeta::jsonbinpack::canonicalizer::rules {
+
+class BooleanAsEnum final
+    : public sourcemeta::jsonbinpack::canonicalizer::Rule {
+public:
+  BooleanAsEnum() : Rule("boolean_as_enum"){};
+  [[nodiscard]] auto
+  condition(const sourcemeta::jsontoolkit::JSON<std::string> &schema) const
+      -> bool override {
+    return sourcemeta::jsontoolkit::schema::has_vocabulary<std::string>(
+               schema,
+               "https://json-schema.org/draft/2020-12/vocab/validation") &&
+           schema.is_object() && schema.contains("type") &&
+           schema.at("type") == "boolean" && !schema.contains("enum");
+  }
+
+  auto transform(sourcemeta::jsontoolkit::JSON<std::string> &schema) const
+      -> void override {
+    schema.assign(
+        "enum",
+        std::vector<sourcemeta::jsontoolkit::JSON<std::string>>{false, true});
+    schema.erase("type");
+  }
+};
+
+} // namespace sourcemeta::jsonbinpack::canonicalizer::rules
