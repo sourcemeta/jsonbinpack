c5c15b75aee4b52ea36b46574d0989766cbecb2d
Juan Cruz Viotti <jv@jviotti.com>
2022-06-09 19:44:24 -0400

    Integrate Doxygen using CMake (#261)
    Fixes: https://github.com/sourcemeta/jsonbinpack/issues/233
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,10 @@
+find_package(Doxygen)
+if(DOXYGEN_FOUND)
+  set(DOXYGEN_IN ${PROJECT_SOURCE_DIR}/Doxyfile.in)
+  set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+  configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+  add_custom_target(doxygen
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    VERBATIM
+    COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT})
+endif()
