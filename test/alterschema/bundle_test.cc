827535b0a890ff8f562f26b348c400d222f562c1
Juan Cruz Viotti <jv@jviotti.com>
2022-06-28 20:23:25 -0400

    Testing altering a simple flat document
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/alterschema/bundle_test.cc
+++ test/alterschema/bundle_test.cc
@@ -1,4 +1,5 @@
 #include <alterschema/bundle.h>
+#include <jsontoolkit/json.h>
 
 #include <gtest/gtest.h>
 #include <memory>    // std::make_unique
@@ -32,4 +33,26 @@ TEST(Bundle, cannot_add_multiple_rules_with_same_name) {
   bundle.add(std::make_unique<ExampleRule1<std::string>>());
   EXPECT_THROW(bundle.add(std::make_unique<ExampleRule1Extra<std::string>>()),
                std::logic_error);
+}
+
+TEST(Bundle, alter_flat_document_no_applicators) {
+  sourcemeta::alterschema::Bundle<std::string> bundle;
+  bundle.add(std::make_unique<ExampleRule1<std::string>>());
+  bundle.add(std::make_unique<ExampleRule2<std::string>>());
+
+  sourcemeta::jsontoolkit::JSON<std::string> document(R"JSON({
+    "foo": "bar",
+    "bar": "baz",
+    "qux": "xxx"
+  })JSON");
+
+  bundle.apply({}, document);
+
+  sourcemeta::jsontoolkit::JSON<std::string> expected(R"JSON({
+    "qux": "xxx"
+  })JSON");
+
+  document.parse();
+  expected.parse();
+  EXPECT_EQ(expected, document);
 }
