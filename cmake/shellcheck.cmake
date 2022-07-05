e6ebeb366881165160027546d5a5e6c744a1d61e
Juan Cruz Viotti <jv@jviotti.com>
2022-07-04 20:14:13 -0400

    Implement a "canonicalize" CLI command (#301)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ cmake/shellcheck.cmake
@@ -0,0 +1,8 @@
+find_program(SHELLCHECK_BIN NAMES shellcheck)
+
+if(SHELLCHECK_BIN)
+  add_custom_target(shellcheck
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    VERBATIM
+    COMMAND ${SHELLCHECK_BIN} ${JSONBINPACK_SH_SOURCE_FILES})
+endif()
