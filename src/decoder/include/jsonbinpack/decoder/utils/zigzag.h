99a8e6cd037df9c5cff8f386cd56d72ce448ccf8
Juan Cruz Viotti <jv@jviotti.com>
2022-08-04 19:22:57 -0400

    Constrain ZigZag encoding/decoding functions to integer types (#344)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/decoder/include/jsonbinpack/decoder/utils/zigzag.h
+++ src/decoder/include/jsonbinpack/decoder/utils/zigzag.h
@@ -1,9 +1,11 @@
 #ifndef SOURCEMETA_JSONBINPACK_DECODER_UTILS_ZIGZAG_H_
 #define SOURCEMETA_JSONBINPACK_DECODER_UTILS_ZIGZAG_H_
 
+#include <type_traits> // std::enable_if_t, std::is_integral_v
+
 namespace sourcemeta::jsonbinpack::decoder::utils {
 
-template <typename T>
+template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
 constexpr auto zigzag_decode(const T value) noexcept -> T {
   if (value % 2 == 0) {
     return value / 2;
--- src/encoder/include/jsonbinpack/encoder/utils/zigzag.h
+++ src/encoder/include/jsonbinpack/encoder/utils/zigzag.h
@@ -1,10 +1,14 @@
 #ifndef SOURCEMETA_JSONBINPACK_ENCODER_UTILS_ZIGZAG_H_
 #define SOURCEMETA_JSONBINPACK_ENCODER_UTILS_ZIGZAG_H_
 
+#include <type_traits> // std::enable_if_t, std::is_integral_v
+
 namespace sourcemeta::jsonbinpack::encoder::utils {
 
-template <typename T>
-constexpr auto zigzag_encode(const T value) noexcept -> T {
+// The SFINAE constrain would allow us to further overload this
+// class for big integers in the future.
+template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+constexpr auto zigzag_encode(const T value) -> T {
   if (value >= 0) {
     return value * 2;
   }
