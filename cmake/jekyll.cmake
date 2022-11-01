f0ba1f26ab1297a522a22fee94c800048f823c11
Juan Cruz Viotti <jv@jviotti.com>
2022-10-31 22:28:25 -0400

    Quote paths in CMake (#373)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/jekyll.cmake
+++ cmake/jekyll.cmake
@@ -1,8 +1,8 @@
 find_program(BUNDLE_BIN NAMES bundle)
 if(BUNDLE_BIN)
   add_custom_target(jekyll
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
     VERBATIM
-    COMMAND ${BUNDLE_BIN} exec jekyll build
-      --source ${JSONBINPACK_WEBSITE_SRC} --destination ${JSONBINPACK_WEBSITE_OUT})
+    COMMAND "${BUNDLE_BIN}" exec jekyll build
+      --source "${JSONBINPACK_WEBSITE_SRC}" --destination "${JSONBINPACK_WEBSITE_OUT}")
 endif()
