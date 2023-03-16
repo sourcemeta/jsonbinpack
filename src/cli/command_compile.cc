a7a34346eb8c8cad901f339d4a7bf59ce968bd6a
Juan Cruz Viotti <jv@jviotti.com>
2023-03-15 20:52:32 -0400

    Make CLI compile again (#405)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/cli/command_compile.cc
+++ src/cli/command_compile.cc
@@ -1,37 +1,33 @@
 #include "commands.h"
-#include "utils.h"
+#include "resolver.h"
 #include <jsonbinpack/mapper/mapper.h>
 #include <jsontoolkit/json.h>
 
 #include <cstdlib>    // EXIT_SUCCESS
 #include <filesystem> // std::filesystem
-#include <iostream>   // std::cout
-#include <sstream>    // std::ostringstream
+#include <fstream>    // std::ifstream
+#include <ios>        // std::ios_base
+#include <iostream>   // std::cin, std::cout, std::endl
 
-static auto compile_document_from_string(const std::string document) -> int {
-  sourcemeta::jsontoolkit::JSON<std::string> schema{document};
-  sourcemeta::jsontoolkit::JSON<std::string> result{
-      sourcemeta::jsonbinpack::map<std::string>(document)};
-  std::cout << result.pretty() << "\n";
+static auto compile_from_json(sourcemeta::jsontoolkit::JSON &schema) -> int {
+  sourcemeta::jsonbinpack::Mapper mapper{
+      sourcemeta::jsonbinpack::cli::Resolver{}};
+  mapper.apply(schema);
+  sourcemeta::jsontoolkit::prettify(schema, std::cout);
+  std::cout << std::endl;
   return EXIT_SUCCESS;
 }
 
 auto sourcemeta::jsonbinpack::cli::compile(
     const std::filesystem::path &schema_path) -> int {
-  // TODO: If JSON could be initialized from an istream, then we could
-  // pipe an istream of the file into JSON directly rather than converting
-  // the entire input document into an std::string first.
-  const std::string raw_schema{
-      sourcemeta::jsonbinpack::cli::utils::read_file(schema_path)};
-  return compile_document_from_string(raw_schema);
+  std::ifstream stream{schema_path};
+  stream.exceptions(std::ios_base::badbit);
+  sourcemeta::jsontoolkit::JSON schema{sourcemeta::jsontoolkit::parse(stream)};
+  return compile_from_json(schema);
 }
 
 auto sourcemeta::jsonbinpack::cli::compile() -> int {
-  std::string line;
-  std::ostringstream stream;
-  while (std::getline(std::cin, line)) {
-    stream << line << "\n";
-  }
-
-  return compile_document_from_string(stream.str());
+  sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(std::cin)};
+  return compile_from_json(schema);
 }
