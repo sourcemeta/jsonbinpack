255fbdf86a4e6440625fccf98ccea67f7f238b47
Juan Cruz Viotti <jv@jviotti.com>
2023-03-24 17:39:14 -0400

    Simplify the `is_boolean_schema` utils definition (#421)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/utils.h
+++ src/canonicalizer/utils.h
@@ -9,38 +9,25 @@
 
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
+// We don't have to check for "type: boolean" as that type
+// is collapsed to an enum by other canonicalizer rules.
 auto is_boolean_schema(
     const sourcemeta::jsontoolkit::Value &schema,
     const std::unordered_map<std::string, bool> &vocabularies) -> bool {
-
-  // If the "type" is set to "boolean" explicitly
-  const bool is_boolean_type =
-      vocabularies.contains(
-          "https://json-schema.org/draft/2020-12/vocab/validation") &&
-      sourcemeta::jsontoolkit::is_object(schema) &&
-      sourcemeta::jsontoolkit::defines(schema, "type") &&
-      sourcemeta::jsontoolkit::is_string(
-          sourcemeta::jsontoolkit::at(schema, "type")) &&
-      sourcemeta::jsontoolkit::to_string(
-          sourcemeta::jsontoolkit::at(schema, "type")) == "boolean";
-
   // If it is an enumeration of booleans
-  const bool is_boolean_enum =
-      vocabularies.contains(
-          "https://json-schema.org/draft/2020-12/vocab/validation") &&
-      sourcemeta::jsontoolkit::is_object(schema) &&
-      sourcemeta::jsontoolkit::defines(schema, "enum") &&
-      sourcemeta::jsontoolkit::is_array(
-          sourcemeta::jsontoolkit::at(schema, "enum")) &&
-      std::all_of(sourcemeta::jsontoolkit::cbegin_array(
-                      sourcemeta::jsontoolkit::at(schema, "enum")),
-                  sourcemeta::jsontoolkit::cend_array(
-                      sourcemeta::jsontoolkit::at(schema, "enum")),
-                  [](const auto &element) {
-                    return sourcemeta::jsontoolkit::is_boolean(element);
-                  });
-
-  return is_boolean_type || is_boolean_enum;
+  return vocabularies.contains(
+             "https://json-schema.org/draft/2020-12/vocab/validation") &&
+         sourcemeta::jsontoolkit::is_object(schema) &&
+         sourcemeta::jsontoolkit::defines(schema, "enum") &&
+         sourcemeta::jsontoolkit::is_array(
+             sourcemeta::jsontoolkit::at(schema, "enum")) &&
+         std::all_of(sourcemeta::jsontoolkit::cbegin_array(
+                         sourcemeta::jsontoolkit::at(schema, "enum")),
+                     sourcemeta::jsontoolkit::cend_array(
+                         sourcemeta::jsontoolkit::at(schema, "enum")),
+                     [](const auto &element) {
+                       return sourcemeta::jsontoolkit::is_boolean(element);
+                     });
 }
 
 } // namespace sourcemeta::jsonbinpack::canonicalizer
