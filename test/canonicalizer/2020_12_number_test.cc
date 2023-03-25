637500a6ba524161ca279707c853f28141b58998
Juan Cruz Viotti <jv@jviotti.com>
2023-03-25 14:52:07 -0400

    Enable misc number-related canonicalizer tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/canonicalizer/2020_12_number_test.cc
+++ test/canonicalizer/2020_12_number_test.cc
@@ -8,6 +8,26 @@
 // A shared instance for the tests
 static Resolver resolver{};
 
+TEST(CanonicalizerNumber_2020_12, implicit_unit_multiple_of_1) {
+  sourcemeta::jsonbinpack::Canonicalizer canonicalizer{resolver};
+
+  sourcemeta::jsontoolkit::JSON schema{sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "integer"
+  })JSON")};
+
+  canonicalizer.apply(schema);
+
+  const sourcemeta::jsontoolkit::JSON expected{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "integer",
+    "multipleOf": 1
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
+
 TEST(CanonicalizerNumber_2020_12, exclusive_maximum_to_maximum_1) {
   sourcemeta::jsonbinpack::Canonicalizer canonicalizer{resolver};
 
@@ -234,6 +254,51 @@ TEST(CanonicalizerNumber_2020_12, exclusive_maximum_to_maximum_10) {
     "type": "integer",
     "multipleOf": 1,
     "maximum": 4.1
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
+
+TEST(CanonicalizerNumber_2020_12, equal_numeric_bounds_as_const_1) {
+  sourcemeta::jsonbinpack::Canonicalizer canonicalizer{resolver};
+
+  sourcemeta::jsontoolkit::JSON schema{sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "integer",
+    "maximum": 5,
+    "minimum": 5
+  })JSON")};
+
+  canonicalizer.apply(schema);
+
+  const sourcemeta::jsontoolkit::JSON expected{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "integer",
+    "multipleOf": 1,
+    "enum": [ 5 ]
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
+
+TEST(CanonicalizerNumber_2020_12, equal_numeric_bounds_as_const_2) {
+  sourcemeta::jsonbinpack::Canonicalizer canonicalizer{resolver};
+
+  sourcemeta::jsontoolkit::JSON schema{sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "number",
+    "maximum": 5.2,
+    "minimum": 5.2
+  })JSON")};
+
+  canonicalizer.apply(schema);
+
+  const sourcemeta::jsontoolkit::JSON expected{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "number",
+    "enum": [ 5.2 ]
   })JSON")};
 
   EXPECT_EQ(schema, expected);
