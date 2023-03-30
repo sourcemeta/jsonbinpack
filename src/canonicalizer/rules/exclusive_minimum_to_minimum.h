9c186afe04bbf948d056a29bfb005eb7e678aa59
Juan Cruz Viotti <jv@jviotti.com>
2023-03-30 19:45:29 +0100

    Simplify exclusiveMaximum/Minimum canonicalizer rules (#436)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/exclusive_minimum_to_minimum.h
+++ src/canonicalizer/rules/exclusive_minimum_to_minimum.h
@@ -20,20 +20,9 @@ public:
 
   auto transform(sourcemeta::jsontoolkit::JSON &document,
                  sourcemeta::jsontoolkit::Value &value) const -> void override {
-    // TODO: Can we extract this logic into a common
-    // sourcemeta::jsontoolkit::plus(document, int) function?
-    const auto new_minimum{
-        sourcemeta::jsontoolkit::is_real(
-            sourcemeta::jsontoolkit::at(value, "exclusiveMinimum"))
-            ? sourcemeta::jsontoolkit::from(
-                  sourcemeta::jsontoolkit::to_real(
-                      sourcemeta::jsontoolkit::at(value, "exclusiveMinimum")) +
-                  1.0)
-            : sourcemeta::jsontoolkit::from(
-                  sourcemeta::jsontoolkit::to_integer(
-                      sourcemeta::jsontoolkit::at(value, "exclusiveMinimum")) +
-                  1)};
-
+    auto new_minimum{sourcemeta::jsontoolkit::from(
+        sourcemeta::jsontoolkit::at(value, "exclusiveMinimum"))};
+    sourcemeta::jsontoolkit::add(new_minimum, sourcemeta::jsontoolkit::from(1));
     sourcemeta::jsontoolkit::assign(document, value, "minimum", new_minimum);
     sourcemeta::jsontoolkit::erase(value, "exclusiveMinimum");
   }
--- vendor/jsontoolkit/include/jsontoolkit/json/write.h
+++ vendor/jsontoolkit/include/jsontoolkit/json/write.h
@@ -7,6 +7,9 @@
 #error Unknown JSON Toolkit backend
 #endif
 
+#include "read.h"
+
+#include <cassert>          // assert
 #include <initializer_list> // std::initializer_list
 #include <iterator>         // std::cbegin, std::cend
 #include <string>           // std::string
@@ -27,6 +30,27 @@ inline auto erase_many(Value &value, std::initializer_list<std::string> keys)
 
 template <typename T> auto erase_many(Value &value, const T &keys) -> void {
   erase_many(value, std::cbegin(keys), std::cend(keys));
+}
+
+inline auto add(JSON &root, Value &value, const Value &additive) -> void {
+  assert(is_number(value));
+  assert(is_number(additive));
+
+  if (is_integer(value) && is_integer(additive)) {
+    const auto new_value{from(to_integer(value) + to_integer(additive))};
+    set(root, value, new_value);
+  } else {
+    const auto new_value{
+        from((is_integer(value) ? static_cast<double>(to_integer(value))
+                                : to_real(value)) +
+             (is_integer(additive) ? static_cast<double>(to_integer(additive))
+                                   : to_real(additive)))};
+    set(root, value, new_value);
+  }
+}
+
+inline auto add(JSON &document, const Value &additive) -> void {
+  add(document, document, additive);
 }
 
 } // namespace sourcemeta::jsontoolkit
