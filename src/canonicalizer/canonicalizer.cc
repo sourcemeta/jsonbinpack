8b7a3e827d2befd930336cca6b52fc44fe5bd785
Juan Cruz Viotti <jv@jviotti.com>
2023-04-02 18:06:54 +0100

    Refactor DuplicateAnyOfBranches canonicalizer rule
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/canonicalizer.cc
+++ src/canonicalizer/canonicalizer.cc
@@ -42,6 +42,7 @@
 #include "rules/drop_non_string_keywords_unevaluated.h"
 #include "rules/drop_non_string_keywords_validation.h"
 #include "rules/duplicate_allof_branches.h"
+#include "rules/duplicate_anyof_branches.h"
 #include "rules/duplicate_oneof_branches.h"
 #include "rules/empty_array_as_const.h"
 #include "rules/empty_dependent_required.h"
@@ -107,6 +108,7 @@ sourcemeta::jsonbinpack::Canonicalizer::Canonicalizer(
   this->bundle.template add<DropNonStringKeywordsUnevaluated>();
   this->bundle.template add<DropNonStringKeywordsValidation>();
   this->bundle.template add<DuplicateAllOfBranches>();
+  this->bundle.template add<DuplicateAnyOfBranches>();
   this->bundle.template add<DuplicateOneOfBranches>();
   this->bundle.template add<EmptyArrayAsConst>();
   this->bundle.template add<EmptyDependentRequired>();
