47d969bd90f3e74ebecd59f66371037392cd4856
Juan Cruz Viotti <jv@jviotti.com>
2023-04-12 11:38:48 -0400

    Maintain global CLI defaults
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/cli/defaults.h
@@ -0,0 +1,9 @@
+#ifndef SOURCEMETA_JSONBINPACK_CLI_DEFAULTS_H_
+#define SOURCEMETA_JSONBINPACK_CLI_DEFAULTS_H_
+
+namespace sourcemeta::jsonbinpack {
+const char *const DEFAULT_METASCHEMA =
+    "https://json-schema.org/draft/2020-12/schema";
+} // namespace sourcemeta::jsonbinpack
+
+#endif
