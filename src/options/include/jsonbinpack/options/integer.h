edce3a272dcf39c57bbe088f0bc96f5aebfce4f2
Juan Cruz Viotti <jv@jviotti.com>
2023-04-23 18:54:50 -0400

    Make options members all `const` (#482)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/options/include/jsonbinpack/options/integer.h
+++ src/options/include/jsonbinpack/options/integer.h
@@ -6,23 +6,23 @@
 namespace sourcemeta::jsonbinpack::options {
 
 struct BoundedMultiplierOptions {
-  std::int64_t minimum;
-  std::int64_t maximum;
-  std::uint64_t multiplier;
+  const std::int64_t minimum;
+  const std::int64_t maximum;
+  const std::uint64_t multiplier;
 };
 
 struct FloorMultiplierOptions {
-  std::int64_t minimum;
-  std::uint64_t multiplier;
+  const std::int64_t minimum;
+  const std::uint64_t multiplier;
 };
 
 struct RoofMultiplierOptions {
-  std::int64_t maximum;
-  std::uint64_t multiplier;
+  const std::int64_t maximum;
+  const std::uint64_t multiplier;
 };
 
 struct MultiplierOptions {
-  std::uint64_t multiplier;
+  const std::uint64_t multiplier;
 };
 
 } // namespace sourcemeta::jsonbinpack::options
