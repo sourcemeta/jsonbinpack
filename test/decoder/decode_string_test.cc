633f5d1743ea2fd77dc2370a34facd193a4042f1
Juan Cruz Viotti <jv@jviotti.com>
2023-04-27 19:41:03 -0400

    Implement decoder for `ROOF_VARINT_PREFIX_UTF8_STRING_SHARED` (#500)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/decoder/decode_string_test.cc
+++ test/decoder/decode_string_test.cc
@@ -41,38 +41,32 @@ TEST(Decoder, FLOOR_VARINT_PREFIX_UTF8_STRING_SHARED_foo_0_foo_3) {
   EXPECT_EQ(result2, expected);
 }
 
-// TEST(Decoder, ROOF_VARINT_PREFIX_UTF8_STRING_SHARED_foo_4) {
-// using namespace sourcemeta::jsonbinpack::decoder;
-// InputByteStream<char> stream{0x02, 0x66, 0x6f, 0x6f};
-// sourcemeta::jsontoolkit::JSON<std::string> result{
-// ROOF_VARINT_PREFIX_UTF8_STRING_SHARED<std::string>(stream, {4})};
-// sourcemeta::jsontoolkit::JSON<std::string> expected{"\"foo\""};
-// result.parse();
-// expected.parse();
-// EXPECT_EQ(result, expected);
-// }
+TEST(Decoder, ROOF_VARINT_PREFIX_UTF8_STRING_SHARED_foo_4) {
+  InputByteStream<char> stream{0x02, 0x66, 0x6f, 0x6f};
+  sourcemeta::jsonbinpack::Decoder decoder{stream};
+  const sourcemeta::jsontoolkit::JSON result{
+      decoder.ROOF_VARINT_PREFIX_UTF8_STRING_SHARED({4})};
+  const sourcemeta::jsontoolkit::JSON expected{
+      sourcemeta::jsontoolkit::from("foo")};
+  EXPECT_EQ(result, expected);
+}
 
-// TEST(Decoder, ROOF_VARINT_PREFIX_UTF8_STRING_SHARED_foo_3_foo_5) {
-// using namespace sourcemeta::jsonbinpack::decoder;
-// InputByteStream<char> stream{0x01, 0x66, 0x6f, 0x6f, 0x00, 0x03, 0x05};
+TEST(Decoder, ROOF_VARINT_PREFIX_UTF8_STRING_SHARED_foo_3_foo_5) {
+  InputByteStream<char> stream{0x01, 0x66, 0x6f, 0x6f, 0x00, 0x03, 0x05};
+  sourcemeta::jsonbinpack::Decoder decoder{stream};
+  const sourcemeta::jsontoolkit::JSON result1{
+      decoder.ROOF_VARINT_PREFIX_UTF8_STRING_SHARED({3})};
+  const sourcemeta::jsontoolkit::JSON expected{
+      sourcemeta::jsontoolkit::from("foo")};
+  EXPECT_EQ(result1, expected);
 
-// sourcemeta::jsontoolkit::JSON<std::string> result_1{
-// ROOF_VARINT_PREFIX_UTF8_STRING_SHARED<std::string>(stream, {3})};
-// result_1.parse();
-// sourcemeta::jsontoolkit::JSON<std::string> expected_1{"\"foo\""};
-// expected_1.parse();
-// EXPECT_EQ(result_1, expected_1);
+  // The cursor is now on the second string
+  EXPECT_EQ(stream.tellg(), 4);
 
-// // The cursor is now on the second string
-// EXPECT_EQ(stream.tellg(), 4);
-
-// sourcemeta::jsontoolkit::JSON<std::string> result_2{
-// ROOF_VARINT_PREFIX_UTF8_STRING_SHARED<std::string>(stream, {5})};
-// result_2.parse();
-// sourcemeta::jsontoolkit::JSON<std::string> expected_2{"\"foo\""};
-// expected_2.parse();
-// EXPECT_EQ(result_2, expected_2);
-// }
+  const sourcemeta::jsontoolkit::JSON result2{
+      decoder.ROOF_VARINT_PREFIX_UTF8_STRING_SHARED({5})};
+  EXPECT_EQ(result2, expected);
+}
 
 // TEST(Decoder, BOUNDED_8BIT_PREFIX_UTF8_STRING_SHARED_foo_3_5) {
 // using namespace sourcemeta::jsonbinpack::decoder;
