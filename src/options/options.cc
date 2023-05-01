4e752864affc19dbffbcd8f8423c469e663fe801
Juan Cruz Viotti <jv@jviotti.com>
2023-05-01 14:36:29 -0400

    Extract options.h implementations into a `.cc` file (#524)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/options/options.cc
@@ -0,0 +1,60 @@
+#include <jsonbinpack/options/options.h>
+
+#include <algorithm> // std::transform
+#include <iterator>  // std::back_inserter
+#include <utility>   // std::move
+
+namespace sourcemeta::jsonbinpack::options {
+
+BYTE_CHOICE_INDEX::BYTE_CHOICE_INDEX(const BYTE_CHOICE_INDEX &other) {
+  std::transform(other.choices.cbegin(), other.choices.cend(),
+                 std::back_inserter(this->choices), [](const auto &json) {
+                   return sourcemeta::jsontoolkit::from(json);
+                 });
+}
+
+BYTE_CHOICE_INDEX::BYTE_CHOICE_INDEX(BYTE_CHOICE_INDEX &&other) noexcept
+    : choices{std::move(other.choices)} {}
+
+BYTE_CHOICE_INDEX::BYTE_CHOICE_INDEX(
+    std::vector<sourcemeta::jsontoolkit::JSON> &&other)
+    : choices{std::move(other)} {}
+
+LARGE_CHOICE_INDEX::LARGE_CHOICE_INDEX(const LARGE_CHOICE_INDEX &other) {
+  std::transform(other.choices.cbegin(), other.choices.cend(),
+                 std::back_inserter(this->choices), [](const auto &json) {
+                   return sourcemeta::jsontoolkit::from(json);
+                 });
+}
+
+LARGE_CHOICE_INDEX::LARGE_CHOICE_INDEX(LARGE_CHOICE_INDEX &&other) noexcept
+    : choices{std::move(other.choices)} {}
+
+LARGE_CHOICE_INDEX::LARGE_CHOICE_INDEX(
+    std::vector<sourcemeta::jsontoolkit::JSON> &&other)
+    : choices{std::move(other)} {}
+
+TOP_LEVEL_BYTE_CHOICE_INDEX::TOP_LEVEL_BYTE_CHOICE_INDEX(
+    const TOP_LEVEL_BYTE_CHOICE_INDEX &other) {
+  std::transform(other.choices.cbegin(), other.choices.cend(),
+                 std::back_inserter(this->choices), [](const auto &json) {
+                   return sourcemeta::jsontoolkit::from(json);
+                 });
+}
+
+TOP_LEVEL_BYTE_CHOICE_INDEX::TOP_LEVEL_BYTE_CHOICE_INDEX(
+    TOP_LEVEL_BYTE_CHOICE_INDEX &&other) noexcept
+    : choices{std::move(other.choices)} {}
+
+TOP_LEVEL_BYTE_CHOICE_INDEX::TOP_LEVEL_BYTE_CHOICE_INDEX(
+    std::vector<sourcemeta::jsontoolkit::JSON> &&other)
+    : choices{std::move(other)} {}
+
+CONST_NONE::CONST_NONE(const sourcemeta::jsontoolkit::JSON &input)
+    : value{sourcemeta::jsontoolkit::from(input)} {}
+CONST_NONE::CONST_NONE(CONST_NONE &&other) noexcept
+    : value{sourcemeta::jsontoolkit::from(other.value)} {}
+CONST_NONE::CONST_NONE(const CONST_NONE &other)
+    : value{sourcemeta::jsontoolkit::from(other.value)} {}
+
+} // namespace sourcemeta::jsonbinpack::options
