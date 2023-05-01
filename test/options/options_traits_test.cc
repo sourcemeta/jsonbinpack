4f3d03617ae6f7a47148ce5368400ac9ebcfd1d2
Juan Cruz Viotti <jv@jviotti.com>
2023-05-01 16:22:38 -0400

    Don't support moving enum encoding options (#528)
    It is hard to move JSON documents. Not defining this by defining the
    copy constructor nicely defaults to copying.
    
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/options/options_traits_test.cc
+++ test/options/options_traits_test.cc
@@ -9,9 +9,9 @@ TEST(Options, encoding_movable) {
   EXPECT_TRUE(std::is_move_constructible_v<Encoding>);
 }
 
-TEST(Options, encoding_nothrow_movable) {
+TEST(Options, encoding_no_nothrow_movable) {
   using namespace sourcemeta::jsonbinpack::options;
-  EXPECT_TRUE(std::is_nothrow_move_constructible_v<Encoding>);
+  EXPECT_FALSE(std::is_nothrow_move_constructible_v<Encoding>);
 }
 
 TEST(Options, encoding_copyable) {
