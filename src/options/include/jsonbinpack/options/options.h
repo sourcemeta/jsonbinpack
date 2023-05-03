227c3a203acc4bdda0242ae107a7f67e025ea5e3
Juan Cruz Viotti <jv@jviotti.com>
2023-05-03 09:02:55 -0500

    Implement `BOUNDED_TYPED_ARRAY` encoding (#536)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/options/include/jsonbinpack/options/options.h
+++ src/options/include/jsonbinpack/options/options.h
@@ -110,6 +110,13 @@ struct BOUNDED_8BITS_TYPED_ARRAY {
   const MultipleEncodings prefix_encodings;
 };
 
+struct BOUNDED_TYPED_ARRAY {
+  const std::uint64_t minimum;
+  const std::uint64_t maximum;
+  const SingleEncoding encoding;
+  const MultipleEncodings prefix_encodings;
+};
+
 // Encoding type
 
 using Encoding = std::variant<
@@ -120,7 +127,7 @@ using Encoding = std::variant<
     FLOOR_VARINT_PREFIX_UTF8_STRING_SHARED,
     ROOF_VARINT_PREFIX_UTF8_STRING_SHARED,
     BOUNDED_8BIT_PREFIX_UTF8_STRING_SHARED, RFC3339_DATE_INTEGER_TRIPLET,
-    FIXED_TYPED_ARRAY, BOUNDED_8BITS_TYPED_ARRAY>;
+    FIXED_TYPED_ARRAY, BOUNDED_8BITS_TYPED_ARRAY, BOUNDED_TYPED_ARRAY>;
 
 // Helper definitions that rely on the Encoding data type
 struct __internal_encoding_wrapper {
