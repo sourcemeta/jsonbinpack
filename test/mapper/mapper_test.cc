48060782b3b16cec6ef76420e8713d2832bd9240
Juan Cruz Viotti <jv@jviotti.com>
2023-05-10 00:56:38 +0100

    Support default dialects in mapper
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/mapper/mapper_test.cc
+++ test/mapper/mapper_test.cc
@@ -17,14 +17,23 @@ TEST(Mapper, unsupported_dialect) {
       std::domain_error);
 }
 
-TEST(Mapper, unknown_dialect) {
+TEST(Mapper, unknown_dialect_default) {
   sourcemeta::jsontoolkit::DefaultResolver resolver;
   sourcemeta::jsonbinpack::Mapper mapper{resolver};
   sourcemeta::jsontoolkit::JSON schema{sourcemeta::jsontoolkit::parse(R"JSON({
-    "type": "boolean"
+    "type": "integer"
   })JSON")};
 
-  EXPECT_THROW(
-      mapper.apply(schema, "https://json-schema.org/draft/2020-12/schema"),
-      std::domain_error);
+  mapper.apply(schema, "https://json-schema.org/draft/2020-12/schema");
+
+  const sourcemeta::jsontoolkit::JSON expected{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
+    "name": "ARBITRARY_MULTIPLE_ZIGZAG_VARINT",
+    "options": {
+      "multiplier": 1
+    }
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
 }
