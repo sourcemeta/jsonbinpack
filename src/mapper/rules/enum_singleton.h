00ce6ff1d8e4dfc2142c815e668e6ff96815fe18
Juan Cruz Viotti <jv@jviotti.com>
2023-05-10 11:27:40 +0100

    Map singleton enumerations to `CONST_NONE`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/mapper/rules/enum_singleton.h
@@ -0,0 +1,35 @@
+namespace sourcemeta::jsonbinpack::mapper {
+
+class EnumSingleton final : public sourcemeta::alterschema::Rule {
+public:
+  EnumSingleton() : Rule("enum_singleton"){};
+
+  [[nodiscard]] auto
+  condition(const sourcemeta::jsontoolkit::Value &schema,
+            const std::string &dialect,
+            const std::unordered_map<std::string, bool> &vocabularies,
+            const std::size_t) const -> bool override {
+    return !is_encoding(schema) &&
+           dialect == "https://json-schema.org/draft/2020-12/schema" &&
+           vocabularies.contains(
+               "https://json-schema.org/draft/2020-12/vocab/validation") &&
+           sourcemeta::jsontoolkit::defines(schema, "enum") &&
+           sourcemeta::jsontoolkit::is_array(
+               sourcemeta::jsontoolkit::at(schema, "enum")) &&
+           sourcemeta::jsontoolkit::size(
+               sourcemeta::jsontoolkit::at(schema, "enum")) == 1;
+  }
+
+  auto transform(sourcemeta::jsontoolkit::JSON &document,
+                 sourcemeta::jsontoolkit::Value &value) const -> void override {
+    auto options{sourcemeta::jsontoolkit::make_object()};
+    sourcemeta::jsontoolkit::assign(
+        options, "value",
+        sourcemeta::jsontoolkit::from(sourcemeta::jsontoolkit::front(
+            sourcemeta::jsontoolkit::at(value, "enum"))));
+
+    make_encoding(document, value, "CONST_NONE", options);
+  }
+};
+
+} // namespace sourcemeta::jsonbinpack::mapper
