d58be37e462494b2ab863802647f4a049d639315
Juan Cruz Viotti <jv@jviotti.com>
2023-05-11 18:11:46 -0400

    Map top-level small enumerations to `TOP_LEVEL_BYTE_CHOICE_INDEX` (#553)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/mapper/2020_12_mapper_enum_test.cc
+++ test/mapper/2020_12_mapper_enum_test.cc
@@ -53,3 +53,28 @@ TEST(MapperEnum_2020_12, const_scalar) {
 
   EXPECT_EQ(schema, expected);
 }
+
+TEST(MapperEnum_2020_12, enum_small_top_level) {
+  sourcemeta::jsontoolkit::DefaultResolver resolver;
+  sourcemeta::jsonbinpack::Canonicalizer canonicalizer{resolver};
+  sourcemeta::jsonbinpack::Mapper mapper{resolver};
+
+  sourcemeta::jsontoolkit::JSON schema{sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "enum": [ 1, 2, 3 ]
+  })JSON")};
+
+  canonicalizer.apply(schema, "https://json-schema.org/draft/2020-12/schema");
+  mapper.apply(schema, "https://json-schema.org/draft/2020-12/schema");
+
+  const sourcemeta::jsontoolkit::JSON expected{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
+    "name": "TOP_LEVEL_BYTE_CHOICE_INDEX",
+    "options": {
+      "choices": [ 1, 2, 3 ]
+    }
+  })JSON")};
+
+  EXPECT_EQ(schema, expected);
+}
