0768f10aa56fe45c2969a253a175315062515614
Juan Cruz Viotti <jv@jviotti.com>
2023-05-13 15:57:51 -0400

    Parse all enumeration encodings (#570)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -12,19 +12,32 @@ auto parse(const sourcemeta::jsontoolkit::Value &input) -> Encoding {
   const auto encoding{sourcemeta::jsontoolkit::to_string(
       sourcemeta::jsontoolkit::at(input, "name"))};
   const auto &options{sourcemeta::jsontoolkit::at(input, "options")};
+
+  // Enumerations
   if (encoding == "BYTE_CHOICE_INDEX") {
     assert(sourcemeta::jsontoolkit::defines(options, "choices"));
     const auto &choices{sourcemeta::jsontoolkit::at(options, "choices")};
     assert(sourcemeta::jsontoolkit::is_array(choices));
     return BYTE_CHOICE_INDEX(sourcemeta::jsontoolkit::to_vector(choices));
+  } else if (encoding == "LARGE_CHOICE_INDEX") {
+    assert(sourcemeta::jsontoolkit::defines(options, "choices"));
+    const auto &choices{sourcemeta::jsontoolkit::at(options, "choices")};
+    assert(sourcemeta::jsontoolkit::is_array(choices));
+    return LARGE_CHOICE_INDEX(sourcemeta::jsontoolkit::to_vector(choices));
+  } else if (encoding == "TOP_LEVEL_BYTE_CHOICE_INDEX") {
+    assert(sourcemeta::jsontoolkit::defines(options, "choices"));
+    const auto &choices{sourcemeta::jsontoolkit::at(options, "choices")};
+    assert(sourcemeta::jsontoolkit::is_array(choices));
+    return TOP_LEVEL_BYTE_CHOICE_INDEX(
+        sourcemeta::jsontoolkit::to_vector(choices));
   } else if (encoding == "CONST_NONE") {
     assert(sourcemeta::jsontoolkit::defines(options, "value"));
     return CONST_NONE(sourcemeta::jsontoolkit::at(options, "value"));
-  } else {
-    std::ostringstream error;
-    error << "Unrecognized encoding: " << encoding;
-    throw std::runtime_error(error.str());
   }
+
+  std::ostringstream error;
+  error << "Unrecognized encoding: " << encoding;
+  throw std::runtime_error(error.str());
 }
 
 } // namespace sourcemeta::jsonbinpack
