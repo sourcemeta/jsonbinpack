c1881a3d9d5532e82cef64a849eba7704b9e7851
Juan Cruz Viotti <jv@jviotti.com>
2023-05-13 15:43:04 -0400

    Parse `BYTE_CHOICE_INDEX` JSON encodings (#568)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -7,10 +7,17 @@
 namespace sourcemeta::jsonbinpack {
 
 auto parse(const sourcemeta::jsontoolkit::Value &input) -> Encoding {
+  assert(sourcemeta::jsontoolkit::defines(input, "name"));
+  assert(sourcemeta::jsontoolkit::defines(input, "options"));
   const auto encoding{sourcemeta::jsontoolkit::to_string(
       sourcemeta::jsontoolkit::at(input, "name"))};
   const auto &options{sourcemeta::jsontoolkit::at(input, "options")};
-  if (encoding == "CONST_NONE") {
+  if (encoding == "BYTE_CHOICE_INDEX") {
+    assert(sourcemeta::jsontoolkit::defines(options, "choices"));
+    const auto &choices{sourcemeta::jsontoolkit::at(options, "choices")};
+    assert(sourcemeta::jsontoolkit::is_array(choices));
+    return BYTE_CHOICE_INDEX(sourcemeta::jsontoolkit::to_vector(choices));
+  } else if (encoding == "CONST_NONE") {
     assert(sourcemeta::jsontoolkit::defines(options, "value"));
     return CONST_NONE(sourcemeta::jsontoolkit::at(options, "value"));
   } else {
