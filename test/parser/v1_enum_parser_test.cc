a3cc9a9ae8d0976466fe7393018e5bd6591c927a
Juan Cruz Viotti <jv@jviotti.com>
2023-05-13 15:08:30 -0400

    Parse `BYTE_CHOICE_INDEX` JSON encodings
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/parser/v1_enum_parser_test.cc
+++ test/parser/v1_enum_parser_test.cc
@@ -4,6 +4,29 @@
 #include <gtest/gtest.h>
 #include <variant>
 
+TEST(Parser_v1, BYTE_CHOICE_INDEX_scalars) {
+  const sourcemeta::jsontoolkit::JSON input{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
+    "name": "BYTE_CHOICE_INDEX",
+    "options": {
+      "choices": [ 1, 2, 3 ]
+    }
+  })JSON")};
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::parse(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(std::holds_alternative<BYTE_CHOICE_INDEX>(result));
+  EXPECT_EQ(std::get<BYTE_CHOICE_INDEX>(result).choices.size(), 3);
+  EXPECT_EQ(std::get<BYTE_CHOICE_INDEX>(result).choices.at(0),
+            sourcemeta::jsontoolkit::from(1));
+  EXPECT_EQ(std::get<BYTE_CHOICE_INDEX>(result).choices.at(1),
+            sourcemeta::jsontoolkit::from(2));
+  EXPECT_EQ(std::get<BYTE_CHOICE_INDEX>(result).choices.at(2),
+            sourcemeta::jsontoolkit::from(3));
+}
+
 TEST(Parser_v1, CONST_NONE_scalar) {
   const sourcemeta::jsontoolkit::JSON input{
       sourcemeta::jsontoolkit::parse(R"JSON({
