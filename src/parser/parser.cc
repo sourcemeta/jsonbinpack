7fc0f80d81734affe7193997464ebbacce50ed08
Juan Cruz Viotti <jv@jviotti.com>
2023-05-13 14:35:53 -0400

    Extract encoding schema parsing logic into its own component
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -1,19 +1,15 @@
-#include <jsonbinpack/encoding/encoding.h>
-#include <jsonbinpack/mapper/encoding.h>
-#include <jsonbinpack/mapper/parser.h>
+#include <jsonbinpack/parser/parser.h>
 
-#include <cassert>   // assert
 #include <sstream>   // std::ostringstream
 #include <stdexcept> // std::runtime_error
 #include <string>    // std::string
 
-namespace sourcemeta::jsonbinpack::mapper {
+namespace sourcemeta::jsonbinpack {
 
 auto parse(const sourcemeta::jsontoolkit::Value &input) -> Encoding {
-  assert(is_encoding(input));
   const auto encoding{sourcemeta::jsontoolkit::to_string(
-      sourcemeta::jsontoolkit::at(input, keywords::name))};
-  const auto &options{sourcemeta::jsontoolkit::at(input, keywords::options)};
+      sourcemeta::jsontoolkit::at(input, "name"))};
+  const auto &options{sourcemeta::jsontoolkit::at(input, "options")};
   if (encoding == "CONST_NONE") {
     assert(sourcemeta::jsontoolkit::defines(options, "value"));
     return CONST_NONE(sourcemeta::jsontoolkit::at(options, "value"));
@@ -24,4 +20,4 @@ auto parse(const sourcemeta::jsontoolkit::Value &input) -> Encoding {
   }
 }
 
-} // namespace sourcemeta::jsonbinpack::mapper
+} // namespace sourcemeta::jsonbinpack
--- test/parser/v1_enum_parser_test.cc
+++ test/parser/v1_enum_parser_test.cc
@@ -1,10 +1,10 @@
-#include <jsonbinpack/mapper/parser.h>
+#include <jsonbinpack/parser/parser.h>
 #include <jsontoolkit/json.h>
 
 #include <gtest/gtest.h>
 #include <variant>
 
-TEST(MapperParser_v1, CONST_NONE_scalar) {
+TEST(Parser_v1, CONST_NONE_scalar) {
   const sourcemeta::jsontoolkit::JSON input{
       sourcemeta::jsontoolkit::parse(R"JSON({
     "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
@@ -14,8 +14,9 @@ TEST(MapperParser_v1, CONST_NONE_scalar) {
     }
   })JSON")};
 
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::parse(input)};
   using namespace sourcemeta::jsonbinpack;
-  const Encoding result{sourcemeta::jsonbinpack::mapper::parse(input)};
   EXPECT_TRUE(std::holds_alternative<CONST_NONE>(result));
   EXPECT_EQ(std::get<CONST_NONE>(result).value,
             sourcemeta::jsontoolkit::from(1));
