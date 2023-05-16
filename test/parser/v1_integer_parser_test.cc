bb96ce8f32dfa1db5a1594ffdfa4aa6b278e261d
Juan Cruz Viotti <jv@jviotti.com>
2023-05-16 19:24:24 -0400

    Parse `BOUNDED_MULTIPLE_8BITS_ENUM_FIXED` encodings
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/parser/v1_integer_parser_test.cc
@@ -0,0 +1,27 @@
+#include <jsonbinpack/parser/parser.h>
+#include <jsontoolkit/json.h>
+
+#include <gtest/gtest.h>
+#include <variant>
+
+TEST(Parser_v1, BOUNDED_MULTIPLE_8BITS_ENUM_FIXED_positive) {
+  const sourcemeta::jsontoolkit::JSON input{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
+    "name": "BOUNDED_MULTIPLE_8BITS_ENUM_FIXED",
+    "options": {
+      "minimum": 0,
+      "maximum": 100,
+      "multiplier": 2
+    }
+  })JSON")};
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::parse(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(
+      std::holds_alternative<BOUNDED_MULTIPLE_8BITS_ENUM_FIXED>(result));
+  EXPECT_EQ(std::get<BOUNDED_MULTIPLE_8BITS_ENUM_FIXED>(result).minimum, 0);
+  EXPECT_EQ(std::get<BOUNDED_MULTIPLE_8BITS_ENUM_FIXED>(result).maximum, 100);
+  EXPECT_EQ(std::get<BOUNDED_MULTIPLE_8BITS_ENUM_FIXED>(result).multiplier, 2);
+}
