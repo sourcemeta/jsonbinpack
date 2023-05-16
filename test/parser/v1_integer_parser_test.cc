e32abe8fe4865a56454b790eb5567ff1bd7db181
Juan Cruz Viotti <jv@jviotti.com>
2023-05-16 19:40:56 -0400

    Parse `FLOOR_MULTIPLE_ENUM_VARINT` encoding (#576)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/parser/v1_integer_parser_test.cc
+++ test/parser/v1_integer_parser_test.cc
@@ -25,3 +25,22 @@ TEST(Parser_v1, BOUNDED_MULTIPLE_8BITS_ENUM_FIXED_positive) {
   EXPECT_EQ(std::get<BOUNDED_MULTIPLE_8BITS_ENUM_FIXED>(result).maximum, 100);
   EXPECT_EQ(std::get<BOUNDED_MULTIPLE_8BITS_ENUM_FIXED>(result).multiplier, 2);
 }
+
+TEST(Parser_v1, FLOOR_MULTIPLE_ENUM_VARINT_positive) {
+  const sourcemeta::jsontoolkit::JSON input{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
+    "name": "FLOOR_MULTIPLE_ENUM_VARINT",
+    "options": {
+      "minimum": 0,
+      "multiplier": 2
+    }
+  })JSON")};
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::parse(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(std::holds_alternative<FLOOR_MULTIPLE_ENUM_VARINT>(result));
+  EXPECT_EQ(std::get<FLOOR_MULTIPLE_ENUM_VARINT>(result).minimum, 0);
+  EXPECT_EQ(std::get<FLOOR_MULTIPLE_ENUM_VARINT>(result).multiplier, 2);
+}
