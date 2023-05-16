d9ead27b20d5cc1838936caa8274fd663c151640
Juan Cruz Viotti <jv@jviotti.com>
2023-05-16 19:32:25 -0400

    Parse `BOUNDED_MULTIPLE_8BITS_ENUM_FIXED` encodings (#575)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -1,5 +1,6 @@
 #include <jsonbinpack/parser/parser.h>
 
+#include <cstdint>   // std::uint64_t
 #include <sstream>   // std::ostringstream
 #include <stdexcept> // std::runtime_error
 #include <string>    // std::string
@@ -13,8 +14,22 @@ auto parse(const sourcemeta::jsontoolkit::Value &input) -> Encoding {
       sourcemeta::jsontoolkit::at(input, "name"))};
   const auto &options{sourcemeta::jsontoolkit::at(input, "options")};
 
-  // Enumerations
-  if (encoding == "BYTE_CHOICE_INDEX") {
+  // Integers
+  if (encoding == "BOUNDED_MULTIPLE_8BITS_ENUM_FIXED") {
+    assert(sourcemeta::jsontoolkit::defines(options, "minimum"));
+    assert(sourcemeta::jsontoolkit::defines(options, "maximum"));
+    assert(sourcemeta::jsontoolkit::defines(options, "multiplier"));
+    const auto &minimum{sourcemeta::jsontoolkit::at(options, "minimum")};
+    const auto &maximum{sourcemeta::jsontoolkit::at(options, "maximum")};
+    const auto &multiplier{sourcemeta::jsontoolkit::at(options, "multiplier")};
+    return BOUNDED_MULTIPLE_8BITS_ENUM_FIXED{
+        sourcemeta::jsontoolkit::to_integer(minimum),
+        sourcemeta::jsontoolkit::to_integer(maximum),
+        static_cast<std::uint64_t>(
+            sourcemeta::jsontoolkit::to_integer(multiplier))};
+
+    // Enumerations
+  } else if (encoding == "BYTE_CHOICE_INDEX") {
     assert(sourcemeta::jsontoolkit::defines(options, "choices"));
     const auto &choices{sourcemeta::jsontoolkit::at(options, "choices")};
     assert(sourcemeta::jsontoolkit::is_array(choices));
