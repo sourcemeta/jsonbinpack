190f6528f8446707bd3432f420ec1448615dafcc
Juan Cruz Viotti <jv@jviotti.com>
2023-05-17 19:58:10 -0400

    Extract array parsing into another file (#591)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -1,23 +1,17 @@
-#include <jsonbinpack/encoding/wrap.h>
 #include <jsonbinpack/parser/parser.h>
 
+#include "v1_array.h"
 #include "v1_enum.h"
 #include "v1_integer.h"
 #include "v1_number.h"
 #include "v1_string.h"
 
-#include <algorithm> // std::transform
-#include <cstdint>   // std::uint64_t
-#include <iterator>  // std::back_inserter
+#include <cassert>   // assert
 #include <sstream>   // std::ostringstream
 #include <stdexcept> // std::runtime_error
-#include <string>    // std::string
-#include <vector>    // std::vector
 
 namespace sourcemeta::jsonbinpack {
 
-// TODO: Reduce the size of this function, by creating a parse function
-// for each encoding (taken its options as arguments)
 auto parse(const sourcemeta::jsontoolkit::Value &input) -> Encoding {
   assert(sourcemeta::jsontoolkit::defines(input, "name"));
   assert(sourcemeta::jsontoolkit::defines(input, "options"));
@@ -63,27 +57,7 @@ auto parse(const sourcemeta::jsontoolkit::Value &input) -> Encoding {
 
     // Arrays
   } else if (encoding == "FIXED_TYPED_ARRAY") {
-    assert(sourcemeta::jsontoolkit::defines(options, "size"));
-    assert(sourcemeta::jsontoolkit::defines(options, "encoding"));
-    assert(sourcemeta::jsontoolkit::defines(options, "prefixEncodings"));
-    const auto &size{sourcemeta::jsontoolkit::at(options, "size")};
-    const auto &array_encoding{
-        sourcemeta::jsontoolkit::at(options, "encoding")};
-    const auto &prefix_encodings{
-        sourcemeta::jsontoolkit::at(options, "prefixEncodings")};
-    assert(sourcemeta::jsontoolkit::is_integer(size));
-    assert(sourcemeta::jsontoolkit::is_positive(size));
-    assert(sourcemeta::jsontoolkit::is_object(array_encoding));
-    assert(sourcemeta::jsontoolkit::is_array(prefix_encodings));
-    std::vector<Encoding> encodings;
-    std::transform(sourcemeta::jsontoolkit::cbegin_array(prefix_encodings),
-                   sourcemeta::jsontoolkit::cend_array(prefix_encodings),
-                   std::back_inserter(encodings),
-                   [](const auto &element) { return parse(element); });
-    assert(encodings.size() == sourcemeta::jsontoolkit::size(prefix_encodings));
-    return FIXED_TYPED_ARRAY{
-        static_cast<std::uint64_t>(sourcemeta::jsontoolkit::to_integer(size)),
-        wrap(parse(array_encoding)), wrap(encodings.begin(), encodings.end())};
+    return parser::v1::FIXED_TYPED_ARRAY(options);
   }
 
   std::ostringstream error;
---
+++ src/parser/v1_array.h
@@ -0,0 +1,43 @@
+#ifndef SOURCEMETA_JSONBINPACK_PARSER_V1_ARRAY_H_
+#define SOURCEMETA_JSONBINPACK_PARSER_V1_ARRAY_H_
+
+#include <jsonbinpack/encoding/encoding.h>
+#include <jsonbinpack/encoding/wrap.h>
+#include <jsonbinpack/parser/parser.h>
+#include <jsontoolkit/json.h>
+
+#include <algorithm> // std::transform
+#include <cassert>   // assert
+#include <cstdint>   // std::uint64_t
+#include <iterator>  // std::back_inserter
+#include <vector>    // std::vector
+
+namespace sourcemeta::jsonbinpack::parser::v1 {
+
+auto FIXED_TYPED_ARRAY(const sourcemeta::jsontoolkit::Value &options)
+    -> Encoding {
+  assert(sourcemeta::jsontoolkit::defines(options, "size"));
+  assert(sourcemeta::jsontoolkit::defines(options, "encoding"));
+  assert(sourcemeta::jsontoolkit::defines(options, "prefixEncodings"));
+  const auto &size{sourcemeta::jsontoolkit::at(options, "size")};
+  const auto &array_encoding{sourcemeta::jsontoolkit::at(options, "encoding")};
+  const auto &prefix_encodings{
+      sourcemeta::jsontoolkit::at(options, "prefixEncodings")};
+  assert(sourcemeta::jsontoolkit::is_integer(size));
+  assert(sourcemeta::jsontoolkit::is_positive(size));
+  assert(sourcemeta::jsontoolkit::is_object(array_encoding));
+  assert(sourcemeta::jsontoolkit::is_array(prefix_encodings));
+  std::vector<Encoding> encodings;
+  std::transform(sourcemeta::jsontoolkit::cbegin_array(prefix_encodings),
+                 sourcemeta::jsontoolkit::cend_array(prefix_encodings),
+                 std::back_inserter(encodings),
+                 [](const auto &element) { return parse(element); });
+  assert(encodings.size() == sourcemeta::jsontoolkit::size(prefix_encodings));
+  return sourcemeta::jsonbinpack::FIXED_TYPED_ARRAY{
+      static_cast<std::uint64_t>(sourcemeta::jsontoolkit::to_integer(size)),
+      wrap(parse(array_encoding)), wrap(encodings.begin(), encodings.end())};
+}
+
+} // namespace sourcemeta::jsonbinpack::parser::v1
+
+#endif
