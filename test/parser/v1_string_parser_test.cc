fc3e4d65270797054e2f1c62852b11f188f202ff
Juan Cruz Viotti <jv@jviotti.com>
2023-05-16 20:30:51 -0400

    Parse remaining string bounded encodings
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/parser/v1_string_parser_test.cc
+++ test/parser/v1_string_parser_test.cc
@@ -20,3 +20,62 @@ TEST(Parser_v1, UTF8_STRING_NO_LENGTH_3) {
   EXPECT_TRUE(std::holds_alternative<UTF8_STRING_NO_LENGTH>(result));
   EXPECT_EQ(std::get<UTF8_STRING_NO_LENGTH>(result).size, 3);
 }
+
+TEST(Parser_v1, FLOOR_VARINT_PREFIX_UTF8_STRING_SHARED_3) {
+  const sourcemeta::jsontoolkit::JSON input{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
+    "name": "FLOOR_VARINT_PREFIX_UTF8_STRING_SHARED",
+    "options": {
+      "minimum": 3
+    }
+  })JSON")};
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::parse(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(
+      std::holds_alternative<FLOOR_VARINT_PREFIX_UTF8_STRING_SHARED>(result));
+  EXPECT_EQ(std::get<FLOOR_VARINT_PREFIX_UTF8_STRING_SHARED>(result).minimum,
+            3);
+}
+
+TEST(Parser_v1, ROOF_VARINT_PREFIX_UTF8_STRING_SHARED_3) {
+  const sourcemeta::jsontoolkit::JSON input{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
+    "name": "ROOF_VARINT_PREFIX_UTF8_STRING_SHARED",
+    "options": {
+      "maximum": 3
+    }
+  })JSON")};
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::parse(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(
+      std::holds_alternative<ROOF_VARINT_PREFIX_UTF8_STRING_SHARED>(result));
+  EXPECT_EQ(std::get<ROOF_VARINT_PREFIX_UTF8_STRING_SHARED>(result).maximum, 3);
+}
+
+TEST(Parser_v1, BOUNDED_8BIT_PREFIX_UTF8_STRING_SHARED_open) {
+  const sourcemeta::jsontoolkit::JSON input{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://www.jsonbinpack.org/schemas/encoding/v1.json",
+    "name": "BOUNDED_8BIT_PREFIX_UTF8_STRING_SHARED",
+    "options": {
+      "minimum": 1,
+      "maximum": 3
+    }
+  })JSON")};
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::parse(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(
+      std::holds_alternative<BOUNDED_8BIT_PREFIX_UTF8_STRING_SHARED>(result));
+  EXPECT_EQ(std::get<BOUNDED_8BIT_PREFIX_UTF8_STRING_SHARED>(result).minimum,
+            1);
+  EXPECT_EQ(std::get<BOUNDED_8BIT_PREFIX_UTF8_STRING_SHARED>(result).maximum,
+            3);
+}
