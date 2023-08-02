acbad43c9ff206d7ac8fb0d30673ab35ed0ac763
Juan Cruz Viotti <jv@jviotti.com>
2023-08-01 21:39:11 -0400

    Document formula for ContentSchemaWithoutContentMediaType (#663)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/content_schema_without_content_media_type.h
+++ src/canonicalizer/rules/content_schema_without_content_media_type.h
@@ -1,6 +1,20 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_superfluous
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                      | Required |
+/// |-----------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/content | Y        |
+///
+/// The JSON Schema Content vocabulary the `contentSchema` keyword, which
+/// is set to a schema that defines the given string-encoded value. However,
+/// the `contentSchema` keyword is only considered if the `contentMediaType`
+/// keyword from the Content vocabulary is also set.
+///
+/// \f[\frac{contentSchema \in dom(S) \land contentMediaType \not\in dom(S)}{S
+/// \mapsto S \setminus \{contentSchema\} }\f]
 class ContentSchemaWithoutContentMediaType final
     : public sourcemeta::alterschema::Rule {
 public:
