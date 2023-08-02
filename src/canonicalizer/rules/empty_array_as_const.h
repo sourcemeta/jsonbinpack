1fc0b96b61cdb84cb0c67b5f8537aca506508b8c
Juan Cruz Viotti <jv@jviotti.com>
2023-08-02 14:03:51 -0400

    Document formula for EmptyArrayAsConst (#678)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/empty_array_as_const.h
+++ src/canonicalizer/rules/empty_array_as_const.h
@@ -1,6 +1,21 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_simplification
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// If the `type` keyword from the Validation vocabulary is set to `array` and
+/// the `maxItems` keyword from the Validation is set to 0, then the only
+/// instance that can possibly match the schema is the empty array.
+///
+/// \f[\frac{S.type = array \land S.maxItems = 0}{S
+/// \mapsto S \cup \{ const \mapsto \langle\rangle \} \setminus \{ maxItems \}
+/// }\f]
+
 class EmptyArrayAsConst final : public sourcemeta::alterschema::Rule {
 public:
   EmptyArrayAsConst() : Rule("empty_array_as_const"){};
