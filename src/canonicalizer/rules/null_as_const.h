307abacab93aa6a330a72d364c686c3c462b322f
Juan Cruz Viotti <jv@jviotti.com>
2023-08-02 19:43:02 -0400

    Document formulas for remaining syntax sugar canonicalizer rules
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/null_as_const.h
+++ src/canonicalizer/rules/null_as_const.h
@@ -1,6 +1,21 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_syntax_sugar
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// If the `type` keyword from the Validation vocabulary is set to `null`, the
+/// only possible matching instance is the `null` value. As such, the same
+/// constraint can be expressed using the `const` keyword.
+///
+/// \f[\frac{S.type = null}{S
+/// \mapsto S \cup \{ const \mapsto null \} \setminus \{ type \}
+/// }\f]
+
 class NullAsConst final : public sourcemeta::alterschema::Rule {
 public:
   NullAsConst() : Rule("null_as_const"){};
