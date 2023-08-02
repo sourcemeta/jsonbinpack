6981af759fb45d87c8f134a0b3786d16ef73ffc7
Juan Cruz Viotti <jv@jviotti.com>
2023-08-02 12:33:18 -0400

    Document rules that drop Applicator keywords (#674)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/drop_non_object_keywords_applicator.h
+++ src/canonicalizer/rules/drop_non_object_keywords_applicator.h
@@ -1,6 +1,26 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_heterogeneous
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/applicator | Y        |
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// If the `type` keyword from the Validation is set to `object` and
+/// the Applicator vocabulary is also in use, then keywords from the
+/// Applicator vocabulary that do not apply to object JSON instances
+/// can be removed.
+///
+/// \f[\frac{S.type = object \land K \cap S \not\in \emptyset }{S
+/// \mapsto S \setminus K }\f]
+///
+/// Where:
+///
+/// \f[K = \{ prefixItems, contains, items \}\f]
+
 class DropNonObjectKeywordsApplicator final
     : public sourcemeta::alterschema::Rule {
 public:
--- src/canonicalizer/rules/drop_non_string_keywords_applicator.h
+++ src/canonicalizer/rules/drop_non_string_keywords_applicator.h
@@ -1,6 +1,29 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_heterogeneous
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/applicator | Y        |
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// If the `type` keyword from the Validation is set to `string` and
+/// the Applicator vocabulary is also in use, then keywords from the
+/// Applicator vocabulary that do not apply to string JSON instances
+/// can be removed.
+///
+/// \f[\frac{S.type = string \land (K_{object} \cup K_{array}) \cap S \not\in
+/// \emptyset }{S \mapsto S \setminus (K_{object} \cup K_{array}) }\f]
+///
+/// Where:
+///
+/// \f[K_{object} = \{properties, patternProperties, additionalProperties,
+/// dependentSchemas, propertyNames\}\f]
+///
+/// \f[K_{array} = \{prefixItems, contains, items\}\f]
+
 class DropNonStringKeywordsApplicator final
     : public sourcemeta::alterschema::Rule {
 public:
