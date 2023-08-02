c8f57b70699e85f91a6f11e3c2736ad5787ae3c9
Juan Cruz Viotti <jv@jviotti.com>
2023-08-02 12:00:11 -0400

    Document rules that drop Validation keywords (#673)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/drop_non_object_keywords_validation.h
+++ src/canonicalizer/rules/drop_non_object_keywords_validation.h
@@ -1,6 +1,31 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_heterogeneous
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// If the `type` keyword from the Validation is set to `object`, then keywords
+/// from the Validation vocabulary that do not apply to object JSON instances
+/// can be removed.
+///
+/// \f[\frac{S.type = object \land (K_{string} \cup K_{number} \cup K_{array})
+/// \cap S \not\in \emptyset }{S \mapsto S \setminus (K_{string} \cup K_{number}
+/// \cup K_{array}) }\f]
+///
+/// Where:
+///
+/// \f[K_{string} = \{minLength, maxLength, pattern\}\f]
+///
+/// \f[K_{number} = \{maximum, exclusiveMinimum, multipleOf, exclusiveMaximum,
+/// minimum\}\f]
+///
+/// \f[K_{array} = \{minItems, maxItems, minContains, maxContains,
+/// uniqueItems\}\f]
+
 class DropNonObjectKeywordsValidation final
     : public sourcemeta::alterschema::Rule {
 public:
