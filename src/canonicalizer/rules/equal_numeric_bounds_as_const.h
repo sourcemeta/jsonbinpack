eb92e926de9124efed16a81f25228c294a507427
Juan Cruz Viotti <jv@jviotti.com>
2023-08-02 19:23:06 -0400

    Document formulas for various other canonicalizer rules
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/equal_numeric_bounds_as_const.h
+++ src/canonicalizer/rules/equal_numeric_bounds_as_const.h
@@ -1,6 +1,23 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_simplification
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// If the `type` keyword from the Validation vocabulary is set to either
+/// `number` or `integer` and the `minimum` and `maximum` keywords from the
+/// Validation keywords are set to the same number, then the only instance that
+/// can possibly match the schema is such number.
+///
+/// \f[\frac{S.type \in \{ number, integer \} \land \{ minimum, maximum \}
+/// \subseteq dom(S) \land S.minimum = S.maximum}{S \mapsto S \cup \{ const
+/// \mapsto S.minimum \} \setminus \{ minimum, maximum \}
+/// }\f]
+
 class EqualNumericBoundsAsConst final : public sourcemeta::alterschema::Rule {
 public:
   EqualNumericBoundsAsConst() : Rule("equal_numeric_bounds_as_const"){};
@@ -13,6 +30,13 @@ public:
            vocabularies.contains(
                "https://json-schema.org/draft/2020-12/vocab/validation") &&
            sourcemeta::jsontoolkit::is_object(schema) &&
+           sourcemeta::jsontoolkit::defines(schema, "type") &&
+           sourcemeta::jsontoolkit::is_string(
+               sourcemeta::jsontoolkit::at(schema, "type")) &&
+           (sourcemeta::jsontoolkit::to_string(
+                sourcemeta::jsontoolkit::at(schema, "type")) == "integer" ||
+            sourcemeta::jsontoolkit::to_string(
+                sourcemeta::jsontoolkit::at(schema, "type")) == "number") &&
            sourcemeta::jsontoolkit::defines(schema, "minimum") &&
            sourcemeta::jsontoolkit::is_number(
                sourcemeta::jsontoolkit::at(schema, "minimum")) &&
