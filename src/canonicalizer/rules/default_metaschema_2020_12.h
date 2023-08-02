2eeedde256be0fc87b174b0f32da36c49bdf5bbb
Juan Cruz Viotti <jv@jviotti.com>
2023-08-01 20:49:35 -0400

    Document the DefaultMetaschema_2020_12 formula (#664)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/default_metaschema_2020_12.h
+++ src/canonicalizer/rules/default_metaschema_2020_12.h
@@ -1,6 +1,20 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_implicit
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                   | Required |
+/// |--------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/core | Y        |
+///
+/// If the 2020-12 draft is in use for the schema does not declare the
+/// `$schema` keyword from the Core vocabulary, then it is sensible
+/// to default to the official 2020-12 metaschema.
+///
+/// \f[\frac{\$schema \not\in dom(s)}{s
+/// \mapsto s \cup \{ \$schema \mapsto
+/// \textsl{https://json-schema.org/draft/2020-12/schema} \} }\f]
 class DefaultMetaschema_2020_12 final : public sourcemeta::alterschema::Rule {
 public:
   DefaultMetaschema_2020_12() : Rule("default_metaschema_2020_12"){};
