fc8f88991ca37828055a9266a44561a1944fb0e9
Juan Cruz Viotti <jv@jviotti.com>
2023-08-02 11:34:04 -0400

    Document rules that drop Content keywords (#671)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/drop_non_numeric_keywords_content.h
+++ src/canonicalizer/rules/drop_non_numeric_keywords_content.h
@@ -1,6 +1,26 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_heterogeneous
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/content    | Y        |
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// If the `type` keyword from the Validation is set to either `number` or
+/// `integer` and the Content vocabulary is also in use, then keywords from the
+/// Content vocabulary that do not apply to numeric JSON instances
+/// can be removed.
+///
+/// \f[\frac{S.type \in \{ number, integer \} \land K \cap S \not\in \emptyset
+/// }{S \mapsto S \setminus K }\f]
+///
+/// Where:
+///
+/// \f[K = \{contentEncoding, contentMediaType, contentSchema\}\f]
+
 class DropNonNumericKeywordsContent final
     : public sourcemeta::alterschema::Rule {
 public:
