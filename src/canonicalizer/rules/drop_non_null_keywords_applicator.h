163bfef59425ee41f098d81e543f5bdd9d162bf8
Juan Cruz Viotti <jv@jviotti.com>
2023-08-02 12:00:38 -0400

    Document rules that drop Applicator keywords
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/drop_non_null_keywords_applicator.h
+++ src/canonicalizer/rules/drop_non_null_keywords_applicator.h
@@ -1,6 +1,29 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_heterogeneous
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/applicator | Y        |
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// If the `type` keyword from the Validation is set to `null` and
+/// the Applicator vocabulary is also in use, then keywords from the
+/// Applicator vocabulary that do not apply to null JSON instances
+/// can be removed.
+///
+/// \f[\frac{S.type = null \land (K_{object} \cup K_{array}) \cap S \not\in
+/// \emptyset }{S \mapsto S \setminus (K_{object} \cup K_{array}) }\f]
+///
+/// Where:
+///
+/// \f[K_{object} = \{properties, patternProperties, additionalProperties,
+/// dependentSchemas, propertyNames\}\f]
+///
+/// \f[K_{array} = \{prefixItems, contains, items\}\f]
+
 class DropNonNullKeywordsApplicator final
     : public sourcemeta::alterschema::Rule {
 public:
