dca963acec8bc80f4298f3502965fd58439d1e5a
Juan Cruz Viotti <jv@jviotti.com>
2023-08-02 20:00:15 -0400

    Document formula for ImplicitUnitMultipleOf
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/canonicalizer/rules/implicit_unit_multiple_of.h
+++ src/canonicalizer/rules/implicit_unit_multiple_of.h
@@ -1,6 +1,22 @@
 namespace sourcemeta::jsonbinpack::canonicalizer {
 
 /// @ingroup canonicalizer_rules_implicit
+///
+/// ### JSON Schema 2020-12
+///
+/// | Vocabulary URI                                         | Required |
+/// |--------------------------------------------------------|----------|
+/// | https://json-schema.org/draft/2020-12/vocab/validation | Y        |
+///
+/// The integer 1 is the unit of multiplication for integers. If the `type`
+/// keyword from the Validation vocabulary is set to `integer` and no multiplier
+/// is set through the `multipleOf` keyword from the Validation vocabulary, the
+/// multiplier is implicitly 1.
+///
+/// \f[\frac{S.type = integer \land multipleOf \not\in dom(S)}{S
+/// \mapsto S \cup \{ multipleOf \mapsto 1 \}
+/// }\f]
+
 class ImplicitUnitMultipleOf final : public sourcemeta::alterschema::Rule {
 public:
   ImplicitUnitMultipleOf() : Rule("implicit_unit_multiple_of"){};
