81de971d14911d185126e2c6e2671b66155bd884
Juan Cruz Viotti <jv@jviotti.com>
2023-08-10 14:53:18 -0400

    Revise Doxygen
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -1,10 +1,10 @@
 find_package(Doxygen)
 if(DOXYGEN_FOUND)
-  set(DOXYGEN_IN "${PROJECT_SOURCE_DIR}/Doxyfile.in")
+  set(DOXYGEN_IN "${PROJECT_SOURCE_DIR}/doxygen/Doxyfile.in")
   set(DOXYGEN_OUT "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile")
   configure_file("${DOXYGEN_IN}" "${DOXYGEN_OUT}" @ONLY)
   add_custom_target(doxygen
-    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
     VERBATIM
     COMMAND "${CMAKE_COMMAND}" -E make_directory "${JSONBINPACK_WEBSITE_OUT}"
     COMMAND "${DOXYGEN_EXECUTABLE}" "${DOXYGEN_OUT}")
