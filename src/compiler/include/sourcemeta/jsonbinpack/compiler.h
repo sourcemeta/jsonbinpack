6cf16c530ea1f3e206b93225646244f77cc1413f
Juan Cruz Viotti <jv@jviotti.com>
2024-02-12 14:50:59 -0400

    Make use of export macros (#749)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/compiler/include/sourcemeta/jsonbinpack/compiler.h
+++ src/compiler/include/sourcemeta/jsonbinpack/compiler.h
@@ -1,6 +1,12 @@
 #ifndef SOURCEMETA_JSONBINPACK_COMPILER_H_
 #define SOURCEMETA_JSONBINPACK_COMPILER_H_
 
+#if defined(__EMSCRIPTEN__) || defined(__Unikraft__)
+#define SOURCEMETA_JSONBINPACK_COMPILER_EXPORT
+#else
+#include "compiler_export.h"
+#endif
+
 #include <sourcemeta/jsontoolkit/json.h>
 #include <sourcemeta/jsontoolkit/jsonschema.h>
 
@@ -9,18 +15,21 @@
 
 namespace sourcemeta::jsonbinpack {
 
+SOURCEMETA_JSONBINPACK_COMPILER_EXPORT
 auto compile(sourcemeta::jsontoolkit::JSON &schema,
              const sourcemeta::jsontoolkit::SchemaWalker &walker,
              const sourcemeta::jsontoolkit::SchemaResolver &resolver,
              const std::optional<std::string> &default_dialect = std::nullopt)
     -> void;
 
+SOURCEMETA_JSONBINPACK_COMPILER_EXPORT
 auto canonicalize(
     sourcemeta::jsontoolkit::JSON &schema,
     const sourcemeta::jsontoolkit::SchemaWalker &walker,
     const sourcemeta::jsontoolkit::SchemaResolver &resolver,
     const std::optional<std::string> &default_dialect = std::nullopt) -> void;
 
+SOURCEMETA_JSONBINPACK_COMPILER_EXPORT
 auto plan(sourcemeta::jsontoolkit::JSON &schema,
           const sourcemeta::jsontoolkit::SchemaWalker &walker,
           const sourcemeta::jsontoolkit::SchemaResolver &resolver,
