b7a1e442667f191258c060f9923a8e213f8dbdf0
Juan Cruz Viotti <jv@jviotti.com>
2024-02-23 10:39:15 -0400

    Update Noa to the latest version
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/noa/cmake/noa/targets/clang-tidy.cmake
+++ vendor/noa/cmake/noa/targets/clang-tidy.cmake
@@ -3,10 +3,31 @@ set(NOA_TARGET_CLANG_TIDY_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
 function(noa_target_clang_tidy)
   cmake_parse_arguments(NOA_TARGET_CLANG_TIDY "REQUIRED" "" "SOURCES" ${ARGN})
 
+  set(CLANG_TIDY_FIND_PATHS "")
+
+  # Locate ClangTidy on default Homebrew installations,
+  # given that the LLVM formula won't symlink `clang-tidy`
+  # to any available path by default.
+  if(APPLE)
+    if(IS_DIRECTORY "/opt/homebrew/Cellar/llvm")
+      set(HOMEBREW_LLVM "/opt/homebrew/Cellar/llvm")
+    elseif(IS_DIRECTORY "/usr/local/Cellar/llvm")
+      set(HOMEBREW_LLVM "/opt/local/Cellar/llvm")
+    endif()
+    if(HOMEBREW_LLVM)
+      file(GLOB llvm_version_paths LIST_DIRECTORIES true "${HOMEBREW_LLVM}/*")
+      foreach(llvm_version_path ${llvm_version_paths})
+        list(APPEND CLANG_TIDY_FIND_PATHS "${llvm_version_path}/bin")
+      endforeach()
+    endif()
+  endif()
+
   if(NOA_TARGET_CLANG_TIDY_REQUIRED)
-    find_program(CLANG_TIDY_BIN NAMES clang-tidy REQUIRED)
+    find_program(CLANG_TIDY_BIN NAMES clang-tidy REQUIRED
+      PATHS ${CLANG_TIDY_FIND_PATHS})
   else()
-    find_program(CLANG_TIDY_BIN NAMES clang-tidy)
+    find_program(CLANG_TIDY_BIN NAMES clang-tidy
+      PATHS ${CLANG_TIDY_FIND_PATHS})
   endif()
 
   # This covers the empty list too
