3d77db89f9aba5cfb8e5b62ad7cf9168e51e86a3
Juan Cruz Viotti <jv@jviotti.com>
2024-09-23 14:33:51 -0400

    Upgrade ClangFormat to v19.1.0
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/compiler/mapper_rules/integer_bounded_multiplier_8_bit.h
+++ src/compiler/mapper_rules/integer_bounded_multiplier_8_bit.h
@@ -5,11 +5,11 @@ public:
       : sourcemeta::alterschema::Rule("integer_bounded_multiplier_8_bit",
                                       "TODO") {};
 
-  [[nodiscard]] auto
-  condition(const sourcemeta::jsontoolkit::JSON &schema,
-            const std::string &dialect,
-            const std::set<std::string> &vocabularies,
-            const sourcemeta::jsontoolkit::Pointer &) const -> bool override {
+  [[nodiscard]] auto condition(const sourcemeta::jsontoolkit::JSON &schema,
+                               const std::string &dialect,
+                               const std::set<std::string> &vocabularies,
+                               const sourcemeta::jsontoolkit::Pointer &) const
+      -> bool override {
     if (is_encoding(schema) ||
         dialect != "https://json-schema.org/draft/2020-12/schema" ||
         !vocabularies.contains(
