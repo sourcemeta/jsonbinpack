5a2eae1de558cf923a006c4f4f8fda36c5f1ee14
Juan Cruz Viotti <jv@jviotti.com>
2024-10-08 14:39:01 -0400

    Make varint declaration private (#837)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/runtime/input_stream_varint_test.cc
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+
+#include <limits> // std::numeric_limits
+
+#include "decode_utils.h"
+
+#include <sourcemeta/jsonbinpack/runtime_input_stream.h>
+
+TEST(JSONBinPack_InputStream, varint_0x01) {
+  InputByteStream<char> stream{0x01};
+  sourcemeta::jsonbinpack::InputStream decoder{stream};
+  EXPECT_EQ(decoder.get_varint(), 1);
+}
+
+TEST(JSONBinPack_InputStream, varint_0x17) {
+  InputByteStream<char> stream{0x17};
+  sourcemeta::jsonbinpack::InputStream decoder{stream};
+  EXPECT_EQ(decoder.get_varint(), 23);
+}
+
+TEST(JSONBinPack_InputStream, varint_0xac_0x02) {
+  InputByteStream<char> stream{{0xac, 0x02}};
+  sourcemeta::jsonbinpack::InputStream decoder{stream};
+  EXPECT_EQ(decoder.get_varint(), 300);
+}
+
+TEST(JSONBinPack_InputStream, varint_0xdf_0x89_0x03) {
+  InputByteStream<char> stream{{0xdf, 0x89, 0x03}};
+  sourcemeta::jsonbinpack::InputStream decoder{stream};
+  EXPECT_EQ(decoder.get_varint(), 50399);
+}
+
+TEST(JSONBinPack_InputStream, varint_0xfe_0xff_0xff_0xff_0x0f) {
+  InputByteStream<char> stream{{0xfe, 0xff, 0xff, 0xff, 0x0f}};
+  sourcemeta::jsonbinpack::InputStream decoder{stream};
+  const std::uint64_t result{decoder.get_varint()};
+  const std::uint64_t expected{4294967294};
+  EXPECT_EQ(result, expected);
+}
+
+TEST(JSONBinPack_InputStream, varint_uint64_max) {
+  // In Base-128: (1)1111111 (1)1111111 (1)1111111 (1)1111111
+  // (1)1111111 (1)1111111 (1)1111111 (1)1111111 (1)1111111 (0)0000001
+  InputByteStream<char> stream{
+      {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01}};
+  sourcemeta::jsonbinpack::InputStream decoder{stream};
+  const std::uint64_t result{decoder.get_varint()};
+  const std::uint64_t expected{18446744073709551615U};
+  EXPECT_EQ(result, expected);
+}
--- test/runtime/output_stream_varint_test.cc
+++ test/runtime/output_stream_varint_test.cc
@@ -3,42 +3,49 @@
 #include <limits> // std::numeric_limits
 
 #include "encode_utils.h"
-#include <sourcemeta/jsonbinpack/runtime_varint.h>
 
-TEST(JSONBinPack_Encoder, varint_1) {
+#include <sourcemeta/jsonbinpack/runtime_output_stream.h>
+
+TEST(JSONBinPack_OutputStream, varint_1) {
   OutputByteStream<char> stream{};
-  sourcemeta::jsonbinpack::varint_encode(stream, 1);
+  sourcemeta::jsonbinpack::OutputStream encoder{stream};
+  encoder.put_varint(1);
   EXPECT_BYTES(stream, {0x01});
 }
 
-TEST(JSONBinPack_Encoder, varint_23) {
+TEST(JSONBinPack_OutputStream, varint_23) {
   OutputByteStream<char> stream{};
-  sourcemeta::jsonbinpack::varint_encode(stream, 23);
+  sourcemeta::jsonbinpack::OutputStream encoder{stream};
+  encoder.put_varint(23);
   EXPECT_BYTES(stream, {0x17});
 }
 
-TEST(JSONBinPack_Encoder, varint_300) {
+TEST(JSONBinPack_OutputStream, varint_300) {
   OutputByteStream<char> stream{};
-  sourcemeta::jsonbinpack::varint_encode(stream, 300);
+  sourcemeta::jsonbinpack::OutputStream encoder{stream};
+  encoder.put_varint(300);
   EXPECT_BYTES(stream, {0xac, 0x02});
 }
 
-TEST(JSONBinPack_Encoder, varint_50399) {
+TEST(JSONBinPack_OutputStream, varint_50399) {
   OutputByteStream<char> stream{};
-  sourcemeta::jsonbinpack::varint_encode(stream, 50399);
+  sourcemeta::jsonbinpack::OutputStream encoder{stream};
+  encoder.put_varint(50399);
   EXPECT_BYTES(stream, {0xdf, 0x89, 0x03});
 }
 
-TEST(JSONBinPack_Encoder, varint_4294967294) {
+TEST(JSONBinPack_OutputStream, varint_4294967294) {
   OutputByteStream<char> stream{};
-  sourcemeta::jsonbinpack::varint_encode(stream, 4294967294);
+  sourcemeta::jsonbinpack::OutputStream encoder{stream};
+  encoder.put_varint(4294967294);
   EXPECT_BYTES(stream, {0xfe, 0xff, 0xff, 0xff, 0x0f});
 }
 
-TEST(JSONBinPack_Encoder, varint_uint64_max) {
+TEST(JSONBinPack_OutputStream, varint_uint64_max) {
   OutputByteStream<char> stream{};
   const std::uint64_t value = std::numeric_limits<std::uint64_t>::max();
-  sourcemeta::jsonbinpack::varint_encode(stream, value);
+  sourcemeta::jsonbinpack::OutputStream encoder{stream};
+  encoder.put_varint(value);
   // The input is 18446744073709551615
   // In hex: 0xff 0xff 0xff 0xff 0xff 0xff 0xff 0xff
   // In binary: 11111111 11111111 11111111 11111111 11111111 11111111 11111111
