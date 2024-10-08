f4129ac7715d48135d914022aac4e2b189d3f431
Juan Cruz Viotti <jv@jviotti.com>
2024-10-08 17:25:13 -0400

    Don't create a whole category for "enum" data types (#844)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/runtime/encode_any_test.cc
+++ test/runtime/encode_any_test.cc
@@ -1,10 +1,238 @@
 #include <gtest/gtest.h>
 
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "encode_utils.h"
+
 #include <sourcemeta/jsonbinpack/runtime.h>
 #include <sourcemeta/jsontoolkit/json.h>
+
+TEST(JSONBinPack_Encoder, BYTE_CHOICE_INDEX_1__1_0_0) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  encoder.BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x00});
+}
+
+TEST(JSONBinPack_Encoder, BYTE_CHOICE_INDEX_1__0_1_0) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  encoder.BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x01});
+}
+
+TEST(JSONBinPack_Encoder, BYTE_CHOICE_INDEX_1__0_0_1) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  encoder.BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x02});
+}
+
+TEST(JSONBinPack_Encoder, BYTE_CHOICE_INDEX_bar__foo_bar_bar) {
+  const sourcemeta::jsontoolkit::JSON document{"bar"};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"foo"});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"bar"});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"bar"});
+  encoder.BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x01});
+}
+
+TEST(JSONBinPack_Encoder, BYTE_CHOICE_INDEX_non_scalar_1) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse("{ \"foo\": 1 }");
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"foo\": 2 }"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{}"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("[ 1, 2, 3 ]"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"foo\": 1 }"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"bar\": 1 }"));
+
+  encoder.BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x03});
+}
+
+TEST(JSONBinPack_Encoder, LARGE_CHOICE_INDEX_1__1_0_0) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  encoder.LARGE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x00});
+}
+
+TEST(JSONBinPack_Encoder, LARGE_CHOICE_INDEX_1__0_1_0) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  encoder.LARGE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x01});
+}
+
+TEST(JSONBinPack_Encoder, LARGE_CHOICE_INDEX_1__0_0_1) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  encoder.LARGE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x02});
+}
+
+TEST(JSONBinPack_Encoder, LARGE_CHOICE_INDEX_bar__foo_bar_bar) {
+  const sourcemeta::jsontoolkit::JSON document{"bar"};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"foo"});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"bar"});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"bar"});
+  encoder.LARGE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x01});
+}
+
+TEST(JSONBinPack_Encoder, LARGE_CHOICE_INDEX_non_scalar_1) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse("{ \"foo\": 1 }");
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"foo\": 2 }"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{}"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("[ 1, 2, 3 ]"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"foo\": 1 }"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"bar\": 1 }"));
+
+  encoder.LARGE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x03});
+}
+
+TEST(JSONBinPack_Encoder, LARGE_CHOICE_INDEX_enum_250) {
+  const sourcemeta::jsontoolkit::JSON document{250};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  for (std::int64_t x = 0; x < 255; x++) {
+    choices.push_back(sourcemeta::jsontoolkit::JSON{x});
+  }
+
+  encoder.LARGE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0xfa, 0x01});
+}
+
+TEST(JSONBinPack_Encoder, TOP_LEVEL_BYTE_CHOICE_INDEX_1__1_0_0) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  encoder.TOP_LEVEL_BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {});
+}
+
+TEST(JSONBinPack_Encoder, TOP_LEVEL_BYTE_CHOICE_INDEX_1__0_1_0) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  encoder.TOP_LEVEL_BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x00});
+}
+
+TEST(JSONBinPack_Encoder, TOP_LEVEL_BYTE_CHOICE_INDEX_1__0_0_1) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{0});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{1});
+  encoder.TOP_LEVEL_BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x01});
+}
+
+TEST(JSONBinPack_Encoder, TOP_LEVEL_BYTE_CHOICE_INDEX_bar__foo_bar_bar) {
+  const sourcemeta::jsontoolkit::JSON document{"bar"};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"foo"});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"bar"});
+  choices.push_back(sourcemeta::jsontoolkit::JSON{"bar"});
+  encoder.TOP_LEVEL_BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x00});
+}
+
+TEST(JSONBinPack_Encoder, TOP_LEVEL_BYTE_CHOICE_INDEX_non_scalar_1) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse("{ \"foo\": 1 }");
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  std::vector<sourcemeta::jsontoolkit::JSON> choices;
+
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"foo\": 2 }"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{}"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("[ 1, 2, 3 ]"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"foo\": 1 }"));
+  choices.push_back(sourcemeta::jsontoolkit::parse("{ \"bar\": 1 }"));
+
+  encoder.TOP_LEVEL_BYTE_CHOICE_INDEX(document, {std::move(choices)});
+  EXPECT_BYTES(stream, {0x02});
+}
+
+TEST(JSONBinPack_Encoder, CONST_NONE_scalar) {
+  const sourcemeta::jsontoolkit::JSON document{1};
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  encoder.CONST_NONE(document, {document});
+  EXPECT_BYTES(stream, {});
+}
+
+TEST(JSONBinPack_Encoder, CONST_NONE_complex) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse("{ \"foo\": 1 }");
+  OutputByteStream<char> stream{};
+  sourcemeta::jsonbinpack::Encoder encoder{stream};
+  encoder.CONST_NONE(document, {document});
+  EXPECT_BYTES(stream, {});
+}
 
 TEST(JSONBinPack_Encoder, ANY_PACKED_TYPE_TAG_BYTE_PREFIX__null) {
   using namespace sourcemeta::jsonbinpack;
--- test/runtime/v1_any_loader_test.cc
+++ test/runtime/v1_any_loader_test.cc
@@ -5,6 +5,93 @@
 
 #include <variant>
 
+TEST(JSONBinPack_Loader_v1, BYTE_CHOICE_INDEX_scalars) {
+  const sourcemeta::jsontoolkit::JSON input =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "tag:sourcemeta.com,2024:jsonbinpack/encoding/v1",
+    "binpackEncoding": "BYTE_CHOICE_INDEX",
+    "binpackOptions": {
+      "choices": [ 1, 2, 3 ]
+    }
+  })JSON");
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::load(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(std::holds_alternative<BYTE_CHOICE_INDEX>(result));
+  EXPECT_EQ(std::get<BYTE_CHOICE_INDEX>(result).choices.size(), 3);
+  EXPECT_EQ(std::get<BYTE_CHOICE_INDEX>(result).choices.at(0),
+            sourcemeta::jsontoolkit::JSON{1});
+  EXPECT_EQ(std::get<BYTE_CHOICE_INDEX>(result).choices.at(1),
+            sourcemeta::jsontoolkit::JSON{2});
+  EXPECT_EQ(std::get<BYTE_CHOICE_INDEX>(result).choices.at(2),
+            sourcemeta::jsontoolkit::JSON{3});
+}
+
+TEST(JSONBinPack_Loader_v1, LARGE_CHOICE_INDEX_scalars) {
+  const sourcemeta::jsontoolkit::JSON input =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "tag:sourcemeta.com,2024:jsonbinpack/encoding/v1",
+    "binpackEncoding": "LARGE_CHOICE_INDEX",
+    "binpackOptions": {
+      "choices": [ 1, 2, 3 ]
+    }
+  })JSON");
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::load(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(std::holds_alternative<LARGE_CHOICE_INDEX>(result));
+  EXPECT_EQ(std::get<LARGE_CHOICE_INDEX>(result).choices.size(), 3);
+  EXPECT_EQ(std::get<LARGE_CHOICE_INDEX>(result).choices.at(0),
+            sourcemeta::jsontoolkit::JSON{1});
+  EXPECT_EQ(std::get<LARGE_CHOICE_INDEX>(result).choices.at(1),
+            sourcemeta::jsontoolkit::JSON{2});
+  EXPECT_EQ(std::get<LARGE_CHOICE_INDEX>(result).choices.at(2),
+            sourcemeta::jsontoolkit::JSON{3});
+}
+
+TEST(JSONBinPack_Loader_v1, TOP_LEVEL_BYTE_CHOICE_INDEX_scalars) {
+  const sourcemeta::jsontoolkit::JSON input =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "tag:sourcemeta.com,2024:jsonbinpack/encoding/v1",
+    "binpackEncoding": "TOP_LEVEL_BYTE_CHOICE_INDEX",
+    "binpackOptions": {
+      "choices": [ 1, 2, 3 ]
+    }
+  })JSON");
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::load(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(std::holds_alternative<TOP_LEVEL_BYTE_CHOICE_INDEX>(result));
+  EXPECT_EQ(std::get<TOP_LEVEL_BYTE_CHOICE_INDEX>(result).choices.size(), 3);
+  EXPECT_EQ(std::get<TOP_LEVEL_BYTE_CHOICE_INDEX>(result).choices.at(0),
+            sourcemeta::jsontoolkit::JSON{1});
+  EXPECT_EQ(std::get<TOP_LEVEL_BYTE_CHOICE_INDEX>(result).choices.at(1),
+            sourcemeta::jsontoolkit::JSON{2});
+  EXPECT_EQ(std::get<TOP_LEVEL_BYTE_CHOICE_INDEX>(result).choices.at(2),
+            sourcemeta::jsontoolkit::JSON{3});
+}
+
+TEST(JSONBinPack_Loader_v1, CONST_NONE_scalar) {
+  const sourcemeta::jsontoolkit::JSON input =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "tag:sourcemeta.com,2024:jsonbinpack/encoding/v1",
+    "binpackEncoding": "CONST_NONE",
+    "binpackOptions": {
+      "value": 1
+    }
+  })JSON");
+
+  const sourcemeta::jsonbinpack::Encoding result{
+      sourcemeta::jsonbinpack::load(input)};
+  using namespace sourcemeta::jsonbinpack;
+  EXPECT_TRUE(std::holds_alternative<CONST_NONE>(result));
+  EXPECT_EQ(std::get<CONST_NONE>(result).value,
+            sourcemeta::jsontoolkit::JSON{1});
+}
+
 TEST(JSONBinPack_Loader_v1, ANY_PACKED_TYPE_TAG_BYTE_PREFIX) {
   const sourcemeta::jsontoolkit::JSON input =
       sourcemeta::jsontoolkit::parse(R"JSON({
