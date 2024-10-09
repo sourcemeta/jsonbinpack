96e809341b84f6abc1bd005aa041fccc6705de9d
Juan Cruz Viotti <jv@jviotti.com>
2024-10-09 12:45:34 -0400

    Test encoding/decoding many documents in a row
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/runtime/decode_test.cc
+++ test/runtime/decode_test.cc
@@ -14,3 +14,19 @@ TEST(JSONBinPack_Decoder, generic_decode_BOUNDED_MULTIPLE_8BITS_ENUM_FIXED) {
   const sourcemeta::jsontoolkit::JSON expected{-5};
   EXPECT_EQ(result, expected);
 }
+
+TEST(JSONBinPack_Decoder, ANY_PACKED_TYPE_TAG_BYTE_PREFIX_many) {
+  using namespace sourcemeta::jsonbinpack;
+  InputByteStream stream{0x15, 0x1d, 0x25};
+  Decoder decoder{stream};
+  ANY_PACKED_TYPE_TAG_BYTE_PREFIX options;
+  const auto result_1 = decoder.read(options);
+  const auto result_2 = decoder.read(options);
+  const auto result_3 = decoder.read(options);
+  const sourcemeta::jsontoolkit::JSON expected_1{1};
+  const sourcemeta::jsontoolkit::JSON expected_2{2};
+  const sourcemeta::jsontoolkit::JSON expected_3{3};
+  EXPECT_EQ(result_1, expected_1);
+  EXPECT_EQ(result_2, expected_2);
+  EXPECT_EQ(result_3, expected_3);
+}
