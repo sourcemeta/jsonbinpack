c455f1492709fa8f499f077b96f8211e597dd25c
Juan Cruz Viotti <jv@jviotti.com>
2025-01-21 14:59:33 -0400

    Don't ship Noa over `vendorpull` (#870)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/alterschema/vendor/noa/cmake/FindGoogleTest.cmake
+++
@@ -1,6 +0,0 @@
-if(NOT GoogleTest_FOUND)
-  set(BUILD_GMOCK ON CACHE BOOL "enable googlemock")
-  set(INSTALL_GTEST OFF CACHE BOOL "disable installation")
-  add_subdirectory("${PROJECT_SOURCE_DIR}/vendor/googletest")
-  set(GoogleTest_FOUND ON)
-endif()
