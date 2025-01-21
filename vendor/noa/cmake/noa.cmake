81702f8cd074e025035b44ca7fc722dff0ac17e1
Juan Cruz Viotti <jv@jviotti.com>
2025-01-21 14:17:28 -0400

    Upgrade Noa to the latest version
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/noa/cmake/noa.cmake
+++ vendor/noa/cmake/noa.cmake
@@ -1,4 +1,4 @@
-set(NOA_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
+set(NOA_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/noa")
 include("${NOA_DIRECTORY}/shim.cmake")
 include("${NOA_DIRECTORY}/variables.cmake")
 include("${NOA_DIRECTORY}/defaults.cmake")
