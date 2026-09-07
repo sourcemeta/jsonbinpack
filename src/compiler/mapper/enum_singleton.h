25cec59cb31553dcad65ec5139e59467dee961db
Juan Cruz Viotti <jv@jviotti.com>
2026-09-07 12:42:40 -0300

    Fix
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/compiler/mapper/enum_singleton.h
+++ src/compiler/mapper/enum_singleton.h
@@ -17,7 +17,7 @@ public:
       -> sourcemeta::blaze::SchemaTransformRule::Result override {
     return location.dialect == "https://json-schema.org/draft/2020-12/schema" &&
            vocabularies.contains(sourcemeta::blaze::SchemaVocabularies::Known::
-                                     JSON_Schema_2020_12_Validation) &&
+                                     JSON_SCHEMA_2020_12_VALIDATION) &&
            schema.is_object() && schema.defines("enum") &&
            schema.at("enum").is_array() && schema.at("enum").size() == 1;
   }
